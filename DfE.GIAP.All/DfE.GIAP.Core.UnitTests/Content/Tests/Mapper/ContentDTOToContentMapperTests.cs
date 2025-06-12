53c8dcd3030270ba0b8da4e8095a24abd323ada1
aahmed-dfe <Aasim.AHMED@EDUCATION.GOV.UK>
2025-06-12 13:44:04 +0100

    extend mapper tests

--- DfE.GIAP.All/DfE.GIAP.Core.UnitTests/Content/Tests/Mapper/ContentDTOToContentMapperTests.cs
+++ DfE.GIAP.All/DfE.GIAP.Core.UnitTests/Content/Tests/Mapper/ContentDTOToContentMapperTests.cs
@@ -22,6 +22,41 @@ public sealed class ContentDTOToContentMapperTests
     }
 
     [Fact]
+    public void ContentDTOToContentMapper_Map_Returns_EmptyTitle_When_DTOTitle_Is_Null()
+    {
+        // Arrange
+        ContentDTOToContentMapper mapper = new();
+        ContentDTO dto = ContentDTOTestDoubles.Generate(1).Single();
+        dto.Title = null;
+
+        // Act
+        Core.Content.Application.Model.Content response = mapper.Map(dto);
+
+        // Assert
+        Assert.NotNull(response);
+        Assert.Equal(string.Empty, response.Title);
+        Assert.Equal(dto.Body, response.Body);
+    }
+
+    [Fact]
+    public void ContentDTOToContentMapper_Map_Returns_EmptyBody_When_DTOBody_Is_Null()
+    {
+        // Arrange
+        ContentDTOToContentMapper mapper = new();
+        ContentDTO dto = ContentDTOTestDoubles.Generate(1).Single();
+        dto.Body = null;
+
+        // Act
+        Core.Content.Application.Model.Content response = mapper.Map(dto);
+
+        // Assert
+        Assert.NotNull(response);
+        Assert.Equal(dto.Title, response.Title);
+        Assert.Equal(string.Empty, response.Body);
+    }
+
+
+    [Fact]
     public void ContentDTOToContentMapper_Map_Returns_MappedContent_When_DTOPassed()
     {
         // Arrange
