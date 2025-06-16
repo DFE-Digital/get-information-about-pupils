83a397241590048b485dbf66a65c062b6dfb26c7
Tony Palmer <Tony.PALMER@EDUCATION.GOV.UK>
2025-06-16 15:31:17 +0100

    remove more boilerplate

--- DfE.GIAP.All/DfE.GIAP.Core.UnitTests/NewsArticles/UseCases/CreateNewsArticle/CreateNewsArticleUseCaseTests.cs
+++ DfE.GIAP.All/DfE.GIAP.Core.UnitTests/NewsArticles/UseCases/CreateNewsArticle/CreateNewsArticleUseCaseTests.cs
@@ -1,7 +1,4 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
-
-using DfE.GIAP.Core.NewsArticles.Application.UseCases.CreateNewsArticle;
+﻿using DfE.GIAP.Core.NewsArticles.Application.UseCases.CreateNewsArticle;
 
 namespace DfE.GIAP.Core.UnitTests.NewsArticles.UseCases.CreateNewsArticle;
 public sealed class CreateNewsArticleUseCaseTests
--- DfE.GIAP.All/DfE.GIAP.Core.UnitTests/TestDoubles/CosmosDbCommandHandlerTestDoubles.cs
+++ DfE.GIAP.All/DfE.GIAP.Core.UnitTests/TestDoubles/CosmosDbCommandHandlerTestDoubles.cs
@@ -1,7 +1,4 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
-
-using Dfe.Data.Common.Infrastructure.Persistence.CosmosDb.Handlers.Command;
+﻿using Dfe.Data.Common.Infrastructure.Persistence.CosmosDb.Handlers.Command;
 using DfE.GIAP.Core.NewsArticles.Infrastructure.Repositories;
 
 namespace DfE.GIAP.Core.UnitTests.TestDoubles;
