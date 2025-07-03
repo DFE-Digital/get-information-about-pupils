d355bb544d1e08b9a4e3d1272bf05d38e3dd07ac
Tony Palmer <Tony.PALMER@EDUCATION.GOV.UK>
2025-07-03 15:21:45 +0100

    add valid test

--- DfE.GIAP.All/DfE.GIAP.Core.UnitTests/NewsArticles/UseCases/UpdateNewsArticle/UpdateNewsArticleUseCaseTests.cs
+++ DfE.GIAP.All/DfE.GIAP.Core.UnitTests/NewsArticles/UseCases/UpdateNewsArticle/UpdateNewsArticleUseCaseTests.cs
@@ -1,4 +1,5 @@
-﻿using DfE.GIAP.Core.NewsArticles.Application.UseCases.UpdateNewsArticle;
+﻿using DfE.GIAP.Core.NewsArticles.Application.UseCases.CreateNewsArticle;
+using DfE.GIAP.Core.NewsArticles.Application.UseCases.UpdateNewsArticle;
 
 namespace DfE.GIAP.Core.UnitTests.NewsArticles.UseCases.UpdateNewsArticle;
 public sealed class UpdateNewsArticleUseCaseTests
@@ -25,5 +26,37 @@ public sealed class UpdateNewsArticleUseCaseTests
         await Assert.ThrowsAsync<ArgumentNullException>(act);
         mockRepository.Verify(
             (useCase) => useCase.UpdateNewsArticleAsync(It.IsAny<NewsArticle>()), Times.Never());
+    }
+
+    [Fact]
+    public async Task HandleRequestAsync_ThrowsNullException_When_NewsArticleIsNull()
+    {
+        Mock<INewsArticleWriteRepository> mockRepository = NewsArticleWriteOnlyRepositoryTestDoubles.Default();
+
+        UpdateNewsArticleUseCase sut = new(mockRepository.Object);
+        UpdateNewsArticleRequest request = new(NewsArticle: null!);
+
+        Func<Task> act = () => sut.HandleRequestAsync(request);
+
+        // Act Assert
+        await Assert.ThrowsAsync<ArgumentNullException>(act);
+        mockRepository.Verify(
+            (useCase) => useCase.UpdateNewsArticleAsync(It.IsAny<NewsArticle>()), Times.Never());
+    }
+
+    [Fact]
+    public async Task HandleRequestAsync_CallsRepository_When_ArticleIsValid()
+    {
+        // Arrange
+        Mock<INewsArticleWriteRepository> mockRepository = NewsArticleWriteOnlyRepositoryTestDoubles.Default();
+        UpdateNewsArticleUseCase sut = new(mockRepository.Object);
+
+        UpdateNewsArticleRequest request = new(NewsArticleTestDoubles.Create());
+
+        await sut.HandleRequestAsync(request: request);
+
+        // Act Assert
+        mockRepository.Verify(
+            (useCase) => useCase.UpdateNewsArticleAsync(It.IsAny<NewsArticle>()), Times.Once());
     }
 }
