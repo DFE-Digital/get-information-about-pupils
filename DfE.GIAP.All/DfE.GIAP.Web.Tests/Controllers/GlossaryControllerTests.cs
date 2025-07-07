3e65ae50b88c6fa598879c951b0abf6b278a4788
Tony Palmer <Tony.PALMER@EDUCATION.GOV.UK>
2025-07-07 11:16:48 +0100

    init work

--- DfE.GIAP.All/DfE.GIAP.Web.Tests/Controllers/GlossaryControllerTests.cs
+++ DfE.GIAP.All/DfE.GIAP.Web.Tests/Controllers/GlossaryControllerTests.cs
@@ -1,7 +1,12 @@
-﻿using DfE.GIAP.Common.AppSettings;
+﻿using System.Security.Claims;
+using DfE.GIAP.Common.AppSettings;
+using DfE.GIAP.Common.Enums;
+using DfE.GIAP.Core.Common.Application;
+using DfE.GIAP.Core.Contents.Application.Models;
+using DfE.GIAP.Core.Contents.Application.UseCases.GetContentByPageKeyUseCase;
 using DfE.GIAP.Domain.Models.Common;
-using DfE.GIAP.Service.Download;
 using DfE.GIAP.Service.Content;
+using DfE.GIAP.Service.Download;
 using DfE.GIAP.Web.Controllers;
 using DfE.GIAP.Web.Tests.TestDoubles;
 using DfE.GIAP.Web.ViewModels;
@@ -10,80 +15,98 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
 using Moq;
 using NSubstitute;
-using System.IO;
-using System.Security.Claims;
-using System.Threading.Tasks;
 using Xunit;
-using DfE.GIAP.Common.Enums;
 
-namespace DfE.GIAP.Web.Tests.Controllers
+namespace DfE.GIAP.Web.Tests.Controllers;
+
+[Trait("Category", "Glossary Controller Unit Tests")]
+public class GlossaryControllerTests : IClassFixture<GlossaryResultsFake>
 {
-    [Trait("Category", "Glossary Controller Unit Tests")]
-    public class GlossaryControllerTests : IClassFixture<GlossaryResultsFake>
+    private readonly GlossaryResultsFake _GlossaryResultsFake;
+    private readonly IContentService _mockContentService = Substitute.For<IContentService>();
+    private readonly IDownloadService _mockDownloadService = Substitute.For<IDownloadService>();
+
+    public GlossaryControllerTests(GlossaryResultsFake GlossaryResultsFake)
     {
-        private readonly GlossaryResultsFake _GlossaryResultsFake;
-        private readonly IContentService _mockContentService = Substitute.For<IContentService>();
-        private readonly IDownloadService _mockDownloadService = Substitute.For<IDownloadService>();
+        _GlossaryResultsFake = GlossaryResultsFake;
+    }
 
-        public GlossaryControllerTests(GlossaryResultsFake GlossaryResultsFake)
+    [Fact]
+    public async Task Index_Returns_ViewModel_When_ContentReceived()
+    {
+        // Arrange
+        ClaimsPrincipal user = new UserClaimsPrincipalFake().GetUserClaimsPrincipal();
+        Content content = ContentTestDoubles.Default();
+        GetContentByPageKeyUseCaseResponse response = new(content);
+
+        Mock<IUseCase<GetContentByPageKeyUseCaseRequest, GetContentByPageKeyUseCaseResponse>> mockUseCase = new();
+        mockUseCase.Setup(
+            (t) => t.HandleRequestAsync(
+                It.IsAny<GetContentByPageKeyUseCaseRequest>()))
+            .ReturnsAsync(response)
+            .Verifiable();
+
+        GlossaryController sut = new(mockUseCase.Object, _mockDownloadService);
+
+        // Act
+        IActionResult result = await sut.Index();
+
+        // Assert
+        mockUseCase.Verify(
+            (t) => t.HandleRequestAsync(
+                It.IsAny<GetContentByPageKeyUseCaseRequest>()), Times.Once);
+
+        ViewResult viewResult = Assert.IsType<ViewResult>(result, exactMatch: false);
+        Assert.NotNull(viewResult);
+
+        GlossaryViewModel? viewModel = viewResult.Model as GlossaryViewModel;
+        Assert.NotNull(viewModel);
+        Assert.Equal(content.Title, viewModel.Response.Title);
+        Assert.Equal(content.Body, viewModel.Response.Body);
+    }
+
+    [Fact]
+    public async Task GlossaryController_GetBulkUploadTemplateFile_Should_Return_File()
+    {
+        // Arrange
+        var user = new UserClaimsPrincipalFake().GetUserClaimsPrincipal();
+        var model = _GlossaryResultsFake.GetMetaDataFile();
+        _mockContentService.GetContent(DocumentType.Glossary).ReturnsForAnyArgs(_GlossaryResultsFake.GetCommonResponseBody());
+        var azureFunctionHeaderDetails = new AzureFunctionHeaderDetails() { ClientId = "123456", SessionId = "654321" };
+        var ms = new MemoryStream();
+        _mockDownloadService.GetGlossaryMetaDataDownFileAsync("", ms, azureFunctionHeaderDetails).Returns(Task.FromResult(model));
+
+        var controller = GetGlossaryController(user);
+
+        // Act
+        var result = await controller.GetBulkUploadTemplateFile("Test.csv").ConfigureAwait(false);
+
+        // Assert
+        Assert.Equal(result.FileDownloadName, model.FileDownloadName);
+        Assert.Equal(result.ContentType, model.ContentType);
+    }
+
+    private GlossaryController GetGlossaryController(ClaimsPrincipal user)
+    {
+        var mockAzureAppSettings = new Mock<IOptions<AzureAppSettings>>();
+        mockAzureAppSettings.Setup(x => x.Value)
+            .Returns(new AzureAppSettings() { IsSessionIdStoredInCookie = false });
+
+        Content content = ContentTestDoubles.Default();
+        GetContentByPageKeyUseCaseResponse response = new(content);
+        Mock<IUseCase<GetContentByPageKeyUseCaseRequest, GetContentByPageKeyUseCaseResponse>> mockUseCase = new();
+        mockUseCase.Setup(
+            (t) => t.HandleRequestAsync(
+                It.IsAny<GetContentByPageKeyUseCaseRequest>()))
+            .ReturnsAsync(response)
+            .Verifiable();
+
+        return new GlossaryController(mockUseCase.Object, _mockDownloadService)
         {
-            _GlossaryResultsFake = GlossaryResultsFake;
-        }
-
-        [Fact]
-        public async Task GlossaryController_Index_Should_Return_Termsdata()
-        {
-            // Arrange
-            var user = new UserClaimsPrincipalFake().GetUserClaimsPrincipal();
-            var model = _GlossaryResultsFake.GetGlossaryDetails();
-            _mockContentService.GetContent(DocumentType.Glossary).ReturnsForAnyArgs(_GlossaryResultsFake.GetCommonResponseBody());
-            _mockDownloadService.GetGlossaryMetaDataDownloadList().ReturnsForAnyArgs(model.MetaDataDownloadList);
-
-            var controller = GetGlossaryController(user);
-
-            // Act
-            var result = await controller.Index().ConfigureAwait(false);
-
-            // Assert
-            var viewResult = Assert.IsAssignableFrom<ViewResult>(result);
-            var viewModel = viewResult.Model as GlossaryViewModel;
-            Assert.Equal(viewModel.Response.Title, model.Response.Title);
-            Assert.Equal(viewModel.Response.Body, model.Response.Body);
-        }
-
-        [Fact]
-        public async Task GlossaryController_GetBulkUploadTemplateFile_Should_Return_File()
-        {
-            // Arrange
-            var user = new UserClaimsPrincipalFake().GetUserClaimsPrincipal();
-            var model = _GlossaryResultsFake.GetMetaDataFile();
-            _mockContentService.GetContent(DocumentType.Glossary).ReturnsForAnyArgs(_GlossaryResultsFake.GetCommonResponseBody());
-            var azureFunctionHeaderDetails = new AzureFunctionHeaderDetails() { ClientId = "123456", SessionId = "654321" };
-            var ms = new MemoryStream();
-            _mockDownloadService.GetGlossaryMetaDataDownFileAsync("", ms, azureFunctionHeaderDetails).Returns(Task.FromResult(model));
-
-            var controller = GetGlossaryController(user);
-
-            // Act
-            var result = await controller.GetBulkUploadTemplateFile("Test.csv").ConfigureAwait(false);
-
-            // Assert
-            Assert.Equal(result.FileDownloadName, model.FileDownloadName);
-            Assert.Equal(result.ContentType, model.ContentType);
-        }
-
-        private GlossaryController GetGlossaryController(ClaimsPrincipal user)
-        {
-            var mockAzureAppSettings = new Mock<IOptions<AzureAppSettings>>();
-            mockAzureAppSettings.Setup(x => x.Value)
-                .Returns(new AzureAppSettings() { IsSessionIdStoredInCookie = false });
-            return new GlossaryController(_mockContentService, _mockDownloadService)
+            ControllerContext = new ControllerContext()
             {
-                ControllerContext = new ControllerContext()
-                {
-                    HttpContext = new DefaultHttpContext() { User = user }
-                }
-            };
-        }
+                HttpContext = new DefaultHttpContext() { User = user }
+            }
+        };
     }
 }
