47d68cd6eb69bd1fecc9578e42e45a27f6cf7069
aahmed-dfe <119260940+aahmed-dfe@users.noreply.github.com>
2025-07-09 23:06:19 +0100

    UpdateNewsArticleUseCase RequestModel instead of NewsArticle (#125)
    * apply request model
    
    * format
    
    * implement tests

--- DfE.GIAP.All/src/DfE.GIAP.Core/NewsArticles/Application/Models/NewsArticle.cs
+++ DfE.GIAP.All/src/DfE.GIAP.Core/NewsArticles/Application/Models/NewsArticle.cs
@@ -14,9 +14,4 @@ public record NewsArticle
     public bool Published { get; set; }
     public bool Archived { get; set; }
     public bool Pinned { get; set; }
-
-    public void WithModifiedNow()
-    {
-        ModifiedDate = DateTime.UtcNow;
-    }
 }
