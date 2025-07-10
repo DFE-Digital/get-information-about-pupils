70b9d70925682f35160d11b10f26abe87dde566d
Tony Palmer <Tony.PALMER@EDUCATION.GOV.UK>
2025-07-10 08:29:09 +0100

    wip

---
+++ DfE.GIAP.All/src/DfE.GIAP.Core/Contents/Application/UseCases/UpdateContent/UpdateContentRequest.cs
@@ -0,0 +1,5 @@
+﻿using DfE.GIAP.Core.Common.Application;
+
+namespace DfE.GIAP.Core.Contents.Application.UseCases.UpdateContent;
+
+public record UpdateContentRequest() : IUseCaseRequest;
---
+++ DfE.GIAP.All/src/DfE.GIAP.Core/Contents/Application/UseCases/UpdateContent/UpdateContentUseCase.cs
@@ -0,0 +1,11 @@
+﻿using DfE.GIAP.Core.Common.Application;
+
+namespace DfE.GIAP.Core.Contents.Application.UseCases.UpdateContent;
+
+public class UpdateContentUseCase : IUseCaseRequestOnly<UpdateContentRequest>
+{
+    public Task HandleRequestAsync(UpdateContentRequest request)
+    {
+        throw new NotImplementedException("");
+    }
+}
