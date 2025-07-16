a32e2083150a1aed81d1ea9fe25513b8eb6f5f19
aahmed-dfe <Aasim.AHMED@EDUCATION.GOV.UK>
2025-07-16 20:52:25 +0100

    aaa

--- DfE.GIAP.All/tests/DfE.GIAP.Core.UnitTests/MyPupils/Domain/ValueObjects/PupilIdTests.cs
+++ DfE.GIAP.All/tests/DfE.GIAP.Core.UnitTests/MyPupils/Domain/ValueObjects/PupilIdTests.cs
@@ -7,19 +7,23 @@ public sealed class PupilIdTests
     [Fact]
     public void Id_ShouldReturnStringRepresentationOfGuid()
     {
+        // Arrange
         const string input = "12345678-1234-1234-1234-1234567890ab";
         PupilId pupilId = new(Guid.Parse(input));
 
+        // Act Assert
         Assert.Equal(input, pupilId.Id);
     }
 
     [Fact]
     public void Equality_ShouldBeBasedOnGuidValue()
     {
+        // Arrange
         Guid guid = Guid.NewGuid();
         PupilId pupilId1 = new(guid);
         PupilId pupilId2 = new(guid);
 
+        // Act Assert
         Assert.Equal(pupilId1, pupilId2);
         Assert.True(pupilId1.Equals(pupilId2));
     }
@@ -27,9 +31,11 @@ public sealed class PupilIdTests
     [Fact]
     public void Inequality_ShouldBeTrue_ForDifferentGuids()
     {
+        // Arrange
         PupilId pupilId1 = new(Guid.NewGuid());
         PupilId pupilId2 = new(Guid.NewGuid());
 
+        // Act Assert
         Assert.NotEqual(pupilId1, pupilId2);
     }
 }
