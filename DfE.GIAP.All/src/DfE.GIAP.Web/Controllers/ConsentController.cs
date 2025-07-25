38b0a5c78f27eb62fbafd4067125e1ebb5f8420a
Tony Palmer <Tony.PALMER@EDUCATION.GOV.UK>
2025-07-25 14:08:58 +0100

    init work

--- DfE.GIAP.All/src/DfE.GIAP.Web/Controllers/ConsentController.cs
+++ DfE.GIAP.All/src/DfE.GIAP.Web/Controllers/ConsentController.cs
@@ -1,7 +1,4 @@
 ﻿using DfE.GIAP.Common.AppSettings;
-using DfE.GIAP.Core.Common.Application;
-using DfE.GIAP.Core.Common.CrossCutting;
-using DfE.GIAP.Core.Contents.Application.UseCases.GetContentByPageKeyUseCase;
 using DfE.GIAP.Web.Constants;
 using DfE.GIAP.Web.Extensions;
 using DfE.GIAP.Web.Middleware;
@@ -19,45 +16,30 @@ public class ConsentController : Controller
     private readonly ISessionProvider _sessionProvider;
     private readonly ICookieProvider _cookieProvider;
     private readonly AzureAppSettings _azureAppSettings;
-    private readonly IUseCase<GetContentByPageKeyUseCaseRequest, GetContentByPageKeyUseCaseResponse> _getContentByPageKeyUseCase;
-    private readonly IMapper<GetContentByPageKeyUseCaseResponse, ConsentViewModel> _contentResponseToViewModelMapper;
-
 
     public ConsentController(
         ISessionProvider sessionProvider,
         IOptions<AzureAppSettings> azureAppSettings,
-        ICookieProvider cookieProvider,
-        IUseCase<GetContentByPageKeyUseCaseRequest, GetContentByPageKeyUseCaseResponse> getContentByPageKeyUseCase,
-        IMapper<GetContentByPageKeyUseCaseResponse, ConsentViewModel> contentResponseToViewModelMapper)
+        ICookieProvider cookieProvider)
     {
         ArgumentNullException.ThrowIfNull(sessionProvider);
         ArgumentNullException.ThrowIfNull(azureAppSettings);
         ArgumentNullException.ThrowIfNull(azureAppSettings.Value);
         ArgumentNullException.ThrowIfNull(cookieProvider);
-        ArgumentNullException.ThrowIfNull(getContentByPageKeyUseCase);
-        ArgumentNullException.ThrowIfNull(contentResponseToViewModelMapper);
         _sessionProvider = sessionProvider;
         _azureAppSettings = azureAppSettings.Value;
         _cookieProvider = cookieProvider;
-        _getContentByPageKeyUseCase = getContentByPageKeyUseCase;
-        _contentResponseToViewModelMapper = contentResponseToViewModelMapper;
     }
 
 
     [AllowWithoutConsent]
     [HttpGet]
-    public async Task<IActionResult> Index()
+    public IActionResult Index()
     {
         if (_azureAppSettings.IsSessionIdStoredInCookie)
             _cookieProvider.Set(CookieKeys.GIAPSessionId, User.GetSessionId());
 
-        const string contentPageKey = "Consent";
-
-        GetContentByPageKeyUseCaseResponse response =
-            await _getContentByPageKeyUseCase.HandleRequestAsync(
-                new GetContentByPageKeyUseCaseRequest(pageKey: contentPageKey));
-
-        return ToViewResult(contentPageKey, response);
+        return View(new ConsentViewModel());
     }
 
     [AllowWithoutConsent]
@@ -73,30 +55,4 @@ public class ConsentController : Controller
         viewModel.HasError = true;
         return View(viewModel);
     }
-
-    private ViewResult ToViewResult(string pageKey, GetContentByPageKeyUseCaseResponse response)
-    {
-        if (response.Content == null)
-        {
-            throw new ArgumentException($"Unable to find content with key {pageKey}");
-        }
-
-        ConsentViewModel model = _contentResponseToViewModelMapper.Map(response);
-        return View(model);
-    }
-
 }
-
-internal sealed class GetContentByPageKeyResponseToConsentViewModelMapper : IMapper<GetContentByPageKeyUseCaseResponse, ConsentViewModel>
-{
-    public ConsentViewModel Map(GetContentByPageKeyUseCaseResponse input)
-    {
-        ArgumentNullException.ThrowIfNull(input);
-        ArgumentNullException.ThrowIfNull(input.Content);
-        return new ConsentViewModel()
-        {
-            Response = input.Content
-        };
-    }
-}
-
