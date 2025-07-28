e40a5c3f935889f06513d7e9110c46e3f074e9a5
Tony Palmer <Tony.PALMER@EDUCATION.GOV.UK>
2025-07-28 13:42:42 +0100

    refactor: hardcode glossary page

--- DfE.GIAP.All/src/DfE.GIAP.Web/Controllers/GlossaryController.cs
+++ DfE.GIAP.All/src/DfE.GIAP.Web/Controllers/GlossaryController.cs
@@ -6,23 +6,16 @@ using DfE.GIAP.Domain.Models.Common;
 using DfE.GIAP.Web.Extensions;
 using DfE.GIAP.Web.Middleware;
 using DfE.GIAP.Core.Models.Glossary;
-using DfE.GIAP.Core.Common.Application;
-using DfE.GIAP.Core.Contents.Application.UseCases.GetContentByPageKeyUseCase;
 
 namespace DfE.GIAP.Web.Controllers;
 
 public class GlossaryController : Controller
 {
     private readonly IDownloadService _downloadService;
-    private readonly IUseCase<GetContentByPageKeyUseCaseRequest, GetContentByPageKeyUseCaseResponse> _getContentByPageKeyUseCase;
 
-    public GlossaryController(
-        IUseCase<GetContentByPageKeyUseCaseRequest, GetContentByPageKeyUseCaseResponse> getContentByPageKeyUseCase,
-        IDownloadService downloadService)
+    public GlossaryController(IDownloadService downloadService)
     {
-        ArgumentNullException.ThrowIfNull(getContentByPageKeyUseCase);
         ArgumentNullException.ThrowIfNull(downloadService);
-        _getContentByPageKeyUseCase = getContentByPageKeyUseCase;
         _downloadService = downloadService;
     }
 
@@ -30,17 +23,12 @@ public class GlossaryController : Controller
     [HttpGet]
     public async Task<IActionResult> Index()
     {
-        const string GlossaryPageKey = "Glossary";
-
-        GetContentByPageKeyUseCaseResponse response =
-           await _getContentByPageKeyUseCase.HandleRequestAsync(
-               new GetContentByPageKeyUseCaseRequest(pageKey: GlossaryPageKey));
-
-        IEnumerable<MetaDataDownload> downloadList = await _downloadService.GetGlossaryMetaDataDownloadList().ConfigureAwait(false);
+        IEnumerable<MetaDataDownload> downloadList = await _downloadService
+            .GetGlossaryMetaDataDownloadList()
+            .ConfigureAwait(false);
 
         GlossaryViewModel model = new()
         {
-            Response = response.Content,
             MetaDataDownloadList = downloadList.OrderByDescending(x => x.Date).ToList()
         };
 
@@ -51,11 +39,11 @@ public class GlossaryController : Controller
     [HttpGet]
     public async Task<FileStreamResult> GetBulkUploadTemplateFile(string name)
     {
-        var ms = new MemoryStream();
-        await _downloadService.GetGlossaryMetaDataDownFileAsync(name, ms, AzureFunctionHeaderDetails.Create(User.GetUserId(), User.GetSessionId()));
+        MemoryStream ms = new();
+        await _downloadService
+            .GetGlossaryMetaDataDownFileAsync(name, ms, AzureFunctionHeaderDetails.Create(User.GetUserId(), User.GetSessionId()));
 
         ms.Position = 0;
-
         return new FileStreamResult(ms, MediaTypeNames.Text.Plain)
         {
             FileDownloadName = name
