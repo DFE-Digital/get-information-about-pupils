271f1801c6c4ba718587737ddc87a69cb0b72a12
aahmed-dfe <Aasim.AHMED@EDUCATION.GOV.UK>
2025-07-29 18:18:23 +0100

    test mapper to user

--- DfE.GIAP.All/tests/DfE.GIAP.SharedTests/TestDoubles/UserDtoTestDoubles.cs
+++ DfE.GIAP.All/tests/DfE.GIAP.SharedTests/TestDoubles/UserDtoTestDoubles.cs
@@ -1,4 +1,5 @@
-﻿using DfE.GIAP.Core.User.Application;
+﻿using DfE.GIAP.Core.MyPupils.Domain.ValueObjects;
+using DfE.GIAP.Core.User.Application;
 using DfE.GIAP.Core.User.Infrastructure.Repository;
 using DfE.GIAP.Core.User.Infrastructure.Repository.Dtos;
 
@@ -7,19 +8,27 @@ public static class UserDtoTestDoubles
 {
     public static UserDto Default() => new()
     {
-        id = "user"
+        id = Guid.NewGuid().ToString()
     };
 
     public static UserDto WithId(UserId id) => WithPupils(id, []);
-
     public static UserDto WithPupils(
         UserId id,
-        IEnumerable<MyPupilsItemDto>? myPupils) => new()
+        IEnumerable<UniquePupilNumber> upns)
+    {
+
+        IEnumerable<MyPupilsItemDto>? myPupils = upns.Select((upn) => new MyPupilsItemDto()
+        {
+            UPN = upn.Value
+        });
+
+        return new()
         {
             id = id.Value,
             MyPupils = new()
             {
                 Pupils = myPupils
-            },
+            }
         };
+    }
 }
