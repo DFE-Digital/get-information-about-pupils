6f3db87666dc9746735c2be850b7e5ca9a5bae2a
aahmed-dfe <Aasim.AHMED@EDUCATION.GOV.UK>
2025-07-29 16:18:57 +0100

    add domain objects for pupil construction

---
+++ DfE.GIAP.All/tests/DfE.GIAP.Core.UnitTests/MyPupils/Domain/ValueObjects/SexTests.cs
@@ -0,0 +1,92 @@
+﻿using DfE.GIAP.Core.MyPupils.Domain.ValueObjects;
+
+namespace DfE.GIAP.Core.UnitTests.MyPupils.Domain.ValueObjects;
+public sealed class SexTests
+{
+    [Theory]
+    [InlineData("M")]
+    [InlineData("male")]
+    [InlineData("MaLE")]
+    [InlineData("m")]
+    public void Constructor_WithValidMaleCharacters_ShouldNormaliseAndStoreCorrectly(string input)
+    {
+        // Act
+        Sex sex = new(input);
+
+        // Assert
+        Assert.Equal(Sex.Male.ToString(), sex.ToString());
+    }
+
+    [Theory]
+    [InlineData("F")]
+    [InlineData("f")]
+    [InlineData("female")]
+    [InlineData("FEmAle")]
+    public void Constructor_WithValidFemaleCharacters_ShouldNormaliseAndStoreCorrectly(string input)
+    {
+        // Act
+        Sex sex = new(input);
+
+        // Assert
+        Assert.Equal(Sex.Female.ToString(), sex.ToString());
+    }
+
+    [Theory]
+    [InlineData("X")]
+    [InlineData("z")]
+    [InlineData(" ")]
+    [InlineData("\n")]
+    [InlineData("\r")]
+    [InlineData("1")]
+    [InlineData(null)]
+    [InlineData("Z")]
+    public void Constructor_WithInvalidCharacters_ShouldReturnEmptyString(string? input)
+    {
+        // Act
+        Sex sex = new(input);
+
+        // Assert
+        Assert.Equal(string.Empty, sex.ToString());
+        // TODO when throwing Assert.Throws<ArgumentException>(act);
+    }
+
+    [Fact]
+    public void Male_ShouldReturnCorrectString()
+    {
+        // Arrange
+        Sex expected = new("M");
+
+        // Act
+        Sex result = Sex.Male;
+
+        // Assert
+        Assert.Equal(expected.ToString(), result.ToString());
+    }
+
+    [Fact]
+    public void Female_ShouldReturnCorrectChar()
+    {
+        // Arrange
+        Sex expected = new("F");
+
+        // Act
+        Sex result = Sex.Female;
+
+        // Assert
+        Assert.Equal(expected.ToString(), result.ToString());
+    }
+
+    [Fact]
+    public void Equality_ShouldWorkForSameSex()
+    {
+        // Act & Assert
+        Assert.Equal(new("M"), Sex.Male);
+    }
+
+    [Fact]
+    public void Inequality_ShouldWorkForDifferentSex()
+    {
+        // Act & Assert
+        Assert.NotEqual(Sex.Male, Sex.Female);
+    }
+}
