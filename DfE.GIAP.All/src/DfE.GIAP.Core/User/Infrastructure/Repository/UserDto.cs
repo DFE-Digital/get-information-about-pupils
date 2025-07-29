f3f3401b4e25afcfb8df845289c89c513009b451
aahmed-dfe <Aasim.AHMED@EDUCATION.GOV.UK>
2025-07-29 08:52:37 +0100

    unused properties

--- DfE.GIAP.All/src/DfE.GIAP.Core/User/Infrastructure/Repository/UserDto.cs
+++ DfE.GIAP.All/src/DfE.GIAP.Core/User/Infrastructure/Repository/UserDto.cs
@@ -3,26 +3,8 @@
 public sealed class UserDto
 {
     public string id { get; set; }
+    // TODO data job to merge into a unique list between PupilList and MyPupilList
     public IEnumerable<MyPupilItemDto> MyPupils { get; set; }
-    //{
-    //    get
-    //    {
-    //        IEnumerable<MyPupilItemDto> pupiList = PupilList?.Select(t => new MyPupilItemDto()
-    //        {
-    //            Id = Guid.NewGuid(),
-    //            PupilId = t
-    //        }) ?? [];
-
-    //        return pupiList.Concat(MyPupilList ?? []);
-    //    }
-    //}
-
-    // TODO data job to merge into a unique list between PupilList and MyPupilList and add a Guid
-    public string[] PupilList { get; set; } = [];
-    public IEnumerable<MyPupilItemDto> MyPupilList { get; set; } = [];
-
-
-    //public string? id { get; set; }
     //public DateTime? CreatedDateTime { get; set; }
     //public DateTime? RecentLoginDateTime { get; set; }
     //public DateTime? PreviousLoginDateTime { get; set; }
