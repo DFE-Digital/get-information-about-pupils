ab60b8a95fe5aff594914bc9c1d91c9b15b05c41
aahmed-dfe <Aasim.AHMED@EDUCATION.GOV.UK>
2025-07-29 07:05:52 +0100

    add areas for passing down authorisationContext into application, domain objects for mypupils that were relevant, mapping

---
+++ DfE.GIAP.All/src/DfE.GIAP.Core/AgeLimit.cs
@@ -0,0 +1,18 @@
+﻿namespace DfE.GIAP.Core;
+public readonly struct AgeLimit
+{
+    public AgeLimit(int low, int high)
+    {
+        ArgumentOutOfRangeException.ThrowIfNegative(low);
+        ArgumentOutOfRangeException.ThrowIfNegative(high);
+        ArgumentOutOfRangeException.ThrowIfGreaterThan(high, 999);
+        Low = low;
+        High = high;
+    }
+
+    public int High { get; }
+    public int Low { get; }
+    public bool IsDefaultLimit => IsLowDefaultedOrNotSet && IsHighDefaultedOrNotSet;
+    private bool IsLowDefaultedOrNotSet => Low == 0;
+    private bool IsHighDefaultedOrNotSet => High == 0;
+}
