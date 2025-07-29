3ec62dc302e3fb407270e3044222c46974e48833
TonyPalmerDfE <tony.palmer@education.gov.uk>
2025-07-29 13:49:41 +0100

    refactor: landing page content (#190)

--- DfE.GIAP.All/src/DfE.GIAP.Web/ViewModels/HomeViewModel.cs
+++
@@ -1,8 +0,0 @@
-﻿using DfE.GIAP.Core.Contents.Application.Models;
-using System.Diagnostics.CodeAnalysis;
-
-[ExcludeFromCodeCoverage]
-public class HomeViewModel
-{
-    public Content LandingResponse { get; set; }
-}
