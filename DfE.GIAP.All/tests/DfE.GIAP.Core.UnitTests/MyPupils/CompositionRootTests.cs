1fe9017ad40c1ea6536ea5dcb8689c603b204d62
aahmed-dfe <119260940+aahmed-dfe@users.noreply.github.com>
2025-07-30 09:18:41 +0100

    feat: MyPupils > GetMyPupilsUseCase  (#195)
    * add application usecase-impl and contracts
    
    * fix semicol
    
    * comment out until aggregateservice
    
    * request tests
    
    * add tests for usecase
    
    * lint
    
    * mapper
    
    * coverage over default options
    
    * extract to test doubles
    
    * extract mock mapper for many into dictionary
    
    * overload for 2 lists
    
    * make public
    
    * use date of birth to encapsulate the format
    
    * guard null

--- DfE.GIAP.All/tests/DfE.GIAP.Core.UnitTests/MyPupils/CompositionRootTests.cs
+++ DfE.GIAP.All/tests/DfE.GIAP.Core.UnitTests/MyPupils/CompositionRootTests.cs
@@ -1,5 +1,9 @@
-﻿using DfE.GIAP.Core.Common.CrossCutting;
+﻿using DfE.GIAP.Core.Common.Application;
+using DfE.GIAP.Core.Common.CrossCutting;
 using DfE.GIAP.Core.MyPupils;
+using DfE.GIAP.Core.MyPupils.Application.UseCases.GetMyPupils.Request;
+using DfE.GIAP.Core.MyPupils.Application.UseCases.GetMyPupils.Response;
+using DfE.GIAP.Core.MyPupils.Domain.Entities;
 using DfE.GIAP.Core.SharedTests;
 using DfE.GIAP.Core.SharedTests.TestDoubles;
 using DfE.GIAP.Core.User.Application.Repository;
@@ -32,6 +36,9 @@ public sealed class CompositionRootTests
 
         // Assert
         Assert.NotNull(provider);
+
+        // Waiting on the AggregateService impl Assert.NotNull(provider.GetService<IUseCase<GetMyPupilsRequest, GetMyPupilsResponse>>());
+        Assert.NotNull(provider.GetService<IMapper<Pupil, PupilDto>>());
 
         Assert.NotNull(provider.GetService<IUserReadOnlyRepository>());
         Assert.NotNull(provider.GetService<IMapper<UserDto, User.Application.User>>());
--- DfE.GIAP.All/tests/DfE.GIAP.SharedTests/TestDoubles/MapperTestDoubles.cs
+++ DfE.GIAP.All/tests/DfE.GIAP.SharedTests/TestDoubles/MapperTestDoubles.cs
@@ -22,3 +22,39 @@ public static class MapperTestDoubles
         return mapper;
     }
 }
+
+public static class MapperTestDoublesExtensions
+{
+    public static Mock<IMapper<TIn, TOut>> MockForMany<TIn, TOut>(
+        this Mock<IMapper<TIn, TOut>> mapper,
+        Dictionary<TIn, TOut> mappings) where TIn : notnull
+    {
+        foreach (KeyValuePair<TIn, TOut> kvp in mappings)
+        {
+            mapper
+                .Setup(m => m.Map(kvp.Key))
+                .Returns(kvp.Value)
+                .Verifiable();
+        }
+
+        return mapper;
+    }
+
+    public static Mock<IMapper<TIn, TOut>> MockForMany<TIn, TOut>(
+        this Mock<IMapper<TIn, TOut>> mapper,
+        List<TIn> inputs,
+        List<TOut> outputs) where TIn : notnull
+    {
+        if (inputs.Count != outputs.Count)
+        {
+            throw new ArgumentException("Inputs and outputs must have the same number of elements.");
+        }
+
+        Dictionary<TIn, TOut> mappings = inputs
+            .Zip(outputs, (input, output) => new { input, output })
+            .ToDictionary(x => x.input, x => x.output);
+
+        return mapper.MockForMany(mappings);
+    }
+}
+
