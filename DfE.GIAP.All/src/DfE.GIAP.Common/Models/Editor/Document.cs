d1440764bb9b15c4686dfaaffd29b67ebefbb340
TonyPalmerDfE <tony.palmer@education.gov.uk>
2025-08-01 09:19:23 +0100

    refactor: move away from generic manage document view model (#203)

--- DfE.GIAP.All/src/DfE.GIAP.Common/Models/Editor/Document.cs
+++ DfE.GIAP.All/src/DfE.GIAP.Common/Models/Editor/Document.cs
@@ -5,11 +5,6 @@ namespace DfE.GIAP.Core.Models.Editor;
 [ExcludeFromCodeCoverage]
 public class Document
 {
-    public int Id { get; set; }
-
     public string DocumentId { get; set; }
-
     public string DocumentName { get; set; }
-
-    public int SortId { get; set; }
 }
