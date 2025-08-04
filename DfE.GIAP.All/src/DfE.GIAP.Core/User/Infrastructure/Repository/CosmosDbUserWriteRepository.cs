d224df0e75321a7fa2a66cf5b90314171d5fe6bb
aahmed-dfe <Aasim.AHMED@EDUCATION.GOV.UK>
2025-08-04 19:27:47 +0100

    add user write repository and abstraction

---
+++ DfE.GIAP.All/src/DfE.GIAP.Core/User/Infrastructure/Repository/CosmosDbUserWriteRepository.cs
@@ -0,0 +1,42 @@
+﻿using Dfe.Data.Common.Infrastructure.Persistence.CosmosDb.Handlers.Command;
+using DfE.GIAP.Core.MyPupils.Domain.ValueObjects;
+using DfE.GIAP.Core.User.Application;
+using DfE.GIAP.Core.User.Application.Repository;
+using DfE.GIAP.Core.User.Infrastructure.Repository.Dtos;
+using Microsoft.Azure.Cosmos;
+
+namespace DfE.GIAP.Core.User.Infrastructure.Repository;
+internal sealed class CosmosDbUserWriteRepository : IUserWriteRepository
+{
+    private readonly ICosmosDbCommandHandler _commandHandler;
+
+    public CosmosDbUserWriteRepository(ICosmosDbCommandHandler commandHandler)
+    {
+        _commandHandler = commandHandler;
+    }
+
+    public async Task SaveMyPupilsAsync(UserId userId, IEnumerable<UniquePupilNumber> updatedPupilIds)
+    {
+        IEnumerable<MyPupilsItemDto> updatedPupils = updatedPupilIds?.Select((upn) => new MyPupilsItemDto()
+        {
+            UPN = upn.Value
+        }) ?? [];
+
+        MyPupilsDto updatedMyPupils = new()
+        {
+            Pupils = updatedPupils
+        };
+
+        UserDto updatedUserProfile = new()
+        {
+            id = userId.Value,
+            MyPupils = updatedMyPupils
+            // TODO: map other fields if needed
+        };
+
+        await _commandHandler.UpsertItemAsync(
+            item: updatedUserProfile,
+            containerKey: "users",
+            partitionKey: new PartitionKey(userId.Value));
+    }
+}
