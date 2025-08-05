e561930fb7cb224ef791255d9157ba9f687d7bce
aahmed-dfe <Aasim.AHMED@EDUCATION.GOV.UK>
2025-08-05 15:56:41 +0100

    pull in trimmed down viewmodels for Index

---
+++ DfE.GIAP.All/src/DfE.GIAP.Web/Controllers/MyPupilList/MyPupilsViewModel.cs
@@ -0,0 +1,35 @@
+﻿namespace DfE.GIAP.Web.Controllers.MyPupilList;
+
+public sealed class MyPupilsViewModel
+{
+    private readonly MyPupilsErrorModel? _error;
+    private const int DEFAULT_PAGE_SIZE = 20;
+    public MyPupilsViewModel(
+        IEnumerable<PupilPresentatationModel> pupils,
+        MyPupilsErrorModel? error = null)
+    {
+        Pupils = pupils;
+        _error = error;
+    }
+
+    public string PageHeading => "My pupil list";
+    public string FormAction => "MyPupilList";
+    public string UniquePupilNumberLabel => "UPN";
+    public IEnumerable<PupilPresentatationModel> Pupils { get; } = [];
+    public IEnumerable<string> SelectedPupils { get; } = [];
+    public bool IsAnyPupilsSelected => SelectAll || SelectedPupils.Any();
+    public bool SelectAll { get; set; } = false;
+
+    public bool IsPreviousPageAvailable => PageNumber > 1;
+    public bool IsMorePageAvailable => Pupils.Count() == DEFAULT_PAGE_SIZE;
+    public bool IsNoPupilsRemaining => !Pupils.Any();
+
+    public int PageNumber { get; set; } = 1;
+    public string SortField { get; set; } = string.Empty;
+    public string SortDirection { get; set; } = string.Empty;
+
+    public bool isRemovedSuccessful => false;
+
+    public string ErrorMessage => _error?.Message ?? string.Empty;
+    public bool IsError => _error is not null;
+}
