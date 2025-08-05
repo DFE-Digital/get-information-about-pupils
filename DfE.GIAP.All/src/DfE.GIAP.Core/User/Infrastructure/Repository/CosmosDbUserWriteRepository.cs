03ab90c1229502644882a8ba48127d92688f3d7a
aahmed-dfe <Aasim.AHMED@EDUCATION.GOV.UK>
2025-08-05 07:37:19 +0100

    add tests for user write repository

--- DfE.GIAP.All/src/DfE.GIAP.Core/User/Infrastructure/Repository/CosmosDbUserWriteRepository.cs
+++ DfE.GIAP.All/src/DfE.GIAP.Core/User/Infrastructure/Repository/CosmosDbUserWriteRepository.cs
@@ -4,39 +4,52 @@ using DfE.GIAP.Core.User.Application;
 using DfE.GIAP.Core.User.Application.Repository;
 using DfE.GIAP.Core.User.Infrastructure.Repository.Dtos;
 using Microsoft.Azure.Cosmos;
+using Microsoft.Extensions.Logging;
 
 namespace DfE.GIAP.Core.User.Infrastructure.Repository;
 internal sealed class CosmosDbUserWriteRepository : IUserWriteRepository
 {
     private readonly ICosmosDbCommandHandler _commandHandler;
+    private readonly ILogger<CosmosDbUserWriteRepository> _logger;
 
-    public CosmosDbUserWriteRepository(ICosmosDbCommandHandler commandHandler)
+    public CosmosDbUserWriteRepository(
+        ICosmosDbCommandHandler commandHandler,
+        ILogger<CosmosDbUserWriteRepository> logger)
     {
         _commandHandler = commandHandler;
+        _logger = logger;
     }
 
     public async Task SaveMyPupilsAsync(UserId userId, IEnumerable<UniquePupilNumber> updatedPupilIds)
     {
-        IEnumerable<MyPupilsItemDto> updatedPupils = updatedPupilIds?.Select((upn) => new MyPupilsItemDto()
+        try
         {
-            UPN = upn.Value
-        }) ?? [];
+            IEnumerable<MyPupilsItemDto> updatedPupils = updatedPupilIds?.Select((upn) => new MyPupilsItemDto()
+            {
+                UPN = upn.Value
+            }) ?? [];
 
-        MyPupilsDto updatedMyPupils = new()
+            MyPupilsDto updatedMyPupils = new()
+            {
+                Pupils = updatedPupils
+            };
+
+            UserDto updatedUserProfile = new()
+            {
+                id = userId.Value,
+                MyPupils = updatedMyPupils
+                // TODO: map other fields if needed
+            };
+
+            await _commandHandler.UpsertItemAsync(
+                item: updatedUserProfile,
+                containerKey: "users",
+                partitionKey: new PartitionKey(userId.Value));
+        }
+        catch(CosmosException)
         {
-            Pupils = updatedPupils
-        };
-
-        UserDto updatedUserProfile = new()
-        {
-            id = userId.Value,
-            MyPupils = updatedMyPupils
-            // TODO: map other fields if needed
-        };
-
-        await _commandHandler.UpsertItemAsync(
-            item: updatedUserProfile,
-            containerKey: "users",
-            partitionKey: new PartitionKey(userId.Value));
+            _logger.LogCritical("{method} Error in saving MyPupilsAsync for user: {userId}", nameof(SaveMyPupilsAsync), userId.Value);
+            throw;
+        }
     }
 }
---
+++ DfE.GIAP.All/tests/DfE.GIAP.Core.UnitTests/MyPupils/Infrastructure/Repository/CosmosDbUserWriteRepositoryTests.cs
@@ -0,0 +1,69 @@
+﻿using Dfe.Data.Common.Infrastructure.Persistence.CosmosDb.Handlers.Command;
+using DfE.GIAP.Core.MyPupils.Domain.ValueObjects;
+using DfE.GIAP.Core.User.Application;
+using DfE.GIAP.Core.User.Infrastructure.Repository.Dtos;
+using DfE.GIAP.Core.User.Infrastructure.Repository;
+using DfE.GIAP.Core.SharedTests.TestDoubles;
+using DfE.GIAP.SharedTests.TestDoubles;
+using Microsoft.Azure.Cosmos;
+
+namespace DfE.GIAP.Core.UnitTests.MyPupils.Infrastructure.Repository;
+public sealed class CosmosDbUserWriteRepositoryTests
+{
+    private const string UsersContainerName = "users";
+
+    [Fact]
+    public async Task SaveMyPupilsAsync_MapsUpnsAndCallsUpsert_WithExpectedDto()
+    {
+        // Arrange
+        Mock<ICosmosDbCommandHandler> commandHandlerDouble = new();
+        InMemoryLogger<CosmosDbUserWriteRepository> inMemoryLogger = LoggerTestDoubles.MockLogger<CosmosDbUserWriteRepository>();
+        CosmosDbUserWriteRepository repository = new(commandHandlerDouble.Object, inMemoryLogger);
+
+        UserId userId = UserIdTestDoubles.Default();
+        List<UniquePupilNumber> upns = UniquePupilNumberTestDoubles.Generate(count: 3);
+
+        // Act
+        await repository.SaveMyPupilsAsync(userId, upns);
+
+        // Assert
+
+        IEnumerable<string> expectedUpnsToWrite = upns.Select(t => t.Value);
+
+        commandHandlerDouble.Verify(handler =>
+            handler.UpsertItemAsync(
+                It.Is<UserDto>(dto => dto.id == userId.Value && dto.MyPupils.Pupils.Select(p => p.UPN).SequenceEqual(expectedUpnsToWrite)),
+                UsersContainerName,
+                It.Is<PartitionKey>((pk) => pk == new PartitionKey(userId.Value)),
+                It.IsAny<CancellationToken>()),
+            Times.Once);
+    }
+
+    [Theory]
+    [MemberData(nameof(EmptyInputs))]
+    public async Task SaveMyPupilsAsync_WithNullUpns_UpsertsEmptyPupilList(IEnumerable<UniquePupilNumber>? upns)
+    {
+        // Arrange
+        Mock<ICosmosDbCommandHandler> commandHandlerDouble = new();
+        InMemoryLogger<CosmosDbUserWriteRepository> inMemoryLogger = LoggerTestDoubles.MockLogger<CosmosDbUserWriteRepository>();
+        CosmosDbUserWriteRepository repository = new(commandHandlerDouble.Object, inMemoryLogger);
+
+        UserId userId = UserIdTestDoubles.Default();
+        
+        // Act
+        await repository.SaveMyPupilsAsync(userId, upns!);
+
+        // Assert
+
+        commandHandlerDouble.Verify(handler =>
+            handler.UpsertItemAsync(
+                It.Is<UserDto>(
+                    (dto) => dto.id == userId.Value && dto.MyPupils.Pupils != null && !dto.MyPupils.Pupils.Any()),
+                UsersContainerName,
+                It.Is<PartitionKey>((pk) => pk == new PartitionKey(userId.Value)),
+                It.IsAny<CancellationToken>()),
+            Times.Once);
+
+    }
+    public static TheoryData<IEnumerable<UniquePupilNumber>?> EmptyInputs() => new(null, []);
+}
