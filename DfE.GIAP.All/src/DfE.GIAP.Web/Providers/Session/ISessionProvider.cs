5b046a55b76a356091ce27d96c38b0628c7b4ab8
aahmed-dfe <119260940+aahmed-dfe@users.noreply.github.com>
2025-08-11 11:12:37 +0100

    refactor: Extend ISessionProvider to take Generic type on contract (#229)
    * extend sessionprovider interface
    
    * adjust tests to factor in the argnullexception is a derived argexcp

--- DfE.GIAP.All/src/DfE.GIAP.Web/Providers/Session/ISessionProvider.cs
+++ DfE.GIAP.All/src/DfE.GIAP.Web/Providers/Session/ISessionProvider.cs
@@ -3,7 +3,9 @@
 public interface ISessionProvider
 {
     void SetSessionValue(string key, string value);
-    string GetSessionValue(string key);
+    void SetSessionValue<T>(string key, T value);
+    string? GetSessionValue(string key);
+    T? GetSessionValueOrDefault<T>(string key);
     void RemoveSessionValue(string key);
     bool ContainsSessionKey(string key);
     void ClearSession();
