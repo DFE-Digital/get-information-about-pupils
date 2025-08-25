fe4dcbe799009da806e8369f2f42938ce0c7b5bc
aahmed-dfe <Aasim.AHMED@EDUCATION.GOV.UK>
2025-08-25 19:05:18 +0100

    separate out interface for pupilsSelectionState

---
+++ DfE.GIAP.All/src/DfE.GIAP.Web/Controllers/MyPupilList/PupilSelectionState/IPupilsSelectionState.cs
@@ -0,0 +1,17 @@
+﻿using DfE.GIAP.Web.Controllers.MyPupilList.PupilSelectionState.Provider.DataTransferObjects;
+
+namespace DfE.GIAP.Web.Controllers.MyPupilList.PupilSelectionState;
+
+public interface IPupilsSelectionState
+{
+    bool IsAllPupilsSelected { get; }
+    bool IsAnyPupilsSelected { get; }
+    bool IsPupilSelected(string upn);
+    IReadOnlyCollection<string> GetSelectedPupils();
+    void AddPupils(IEnumerable<string> upns);
+    void SelectAllPupils();
+    void DeselectAllPupils();
+    void UpdatePupilSelectionState(IEnumerable<string> upns, bool isSelected);
+    void ResetState();
+    PupilSelectionStateDto ToDto();
+}
--- DfE.GIAP.All/src/DfE.GIAP.Web/Controllers/MyPupilList/PupilSelectionState/PupilsSelectionState.cs
+++ DfE.GIAP.All/src/DfE.GIAP.Web/Controllers/MyPupilList/PupilSelectionState/PupilsSelectionState.cs
@@ -101,18 +101,3 @@ public sealed class PupilsSelectionState : IPupilsSelectionState
         return state;
     }
 }
-
-
-public interface IPupilsSelectionState
-{
-    bool IsAllPupilsSelected { get; }
-    bool IsAnyPupilsSelected { get; }
-    bool IsPupilSelected(string upn);
-    IReadOnlyCollection<string> GetSelectedPupils();
-    void AddPupils(IEnumerable<string> upns);
-    void SelectAllPupils();
-    void DeselectAllPupils();
-    void UpdatePupilSelectionState(IEnumerable<string> upns, bool isSelected);
-    void ResetState();
-    PupilSelectionStateDto ToDto();
-}
