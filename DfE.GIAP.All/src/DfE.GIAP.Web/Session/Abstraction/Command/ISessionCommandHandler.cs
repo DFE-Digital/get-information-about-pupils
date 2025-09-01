064e8484eff586dbcff4e2e78f3785d1fc195133
aahmed-dfe <Aasim.AHMED@EDUCATION.GOV.UK>
2025-09-01 17:21:10 +0100

    constrain to reference types for now, extend compositionroot tests

--- DfE.GIAP.All/src/DfE.GIAP.Web/Session/Abstraction/Command/ISessionCommandHandler.cs
+++ DfE.GIAP.All/src/DfE.GIAP.Web/Session/Abstraction/Command/ISessionCommandHandler.cs
@@ -1,6 +1,6 @@
 ﻿namespace DfE.GIAP.Web.Session.Abstraction.Command;
 
-public interface ISessionCommandHandler<in TValue>
+public interface ISessionCommandHandler<in TValue> where TValue : class
 {
     void StoreInSession(TValue value);
 }
