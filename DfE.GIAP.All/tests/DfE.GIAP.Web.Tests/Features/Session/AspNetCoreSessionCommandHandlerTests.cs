3d8fb0ae3b477a331cbbb8266df89960d853e3c1
aahmed-dfe <Aasim.AHMED@EDUCATION.GOV.UK>
2025-09-01 09:38:01 +0100

    implement test for command handler paths

--- DfE.GIAP.All/tests/DfE.GIAP.Web.Tests/Features/Session/AspNetCoreSessionCommandHandlerTests.cs
+++ DfE.GIAP.All/tests/DfE.GIAP.Web.Tests/Features/Session/AspNetCoreSessionCommandHandlerTests.cs
@@ -1,6 +1,7 @@
 ﻿using DfE.GIAP.Web.Features.Session.Abstractions;
 using DfE.GIAP.Web.Features.Session.Infrastructure.AspNetCore;
 using DfE.GIAP.Web.Features.Session.Infrastructure.AspNetCore.Command;
+using Microsoft.AspNetCore.Http;
 using Moq;
 using Xunit;
 
@@ -44,6 +45,58 @@ public sealed class AspNetCoreSessionCommandHandlerTests
 
         // Act Assert
         Assert.Throws<ArgumentNullException>(construct);
+    }
+
+    [Fact]
+    public void StoreInSession_Throws_When_Value_Is_Null()
+    {
+        // Arrange
+        Mock<ISessionObjectKeyResolver> keyResolver = new();
+        Mock<IAspNetCoreSessionProvider> sessionProvider = new();
+        Mock<ISessionObjectSerializer<StubSessionObject>> sessionObjectSerializer = new();
+
+        AspNetCoreSessionCommandHandler<StubSessionObject> sut = new(sessionProvider.Object, keyResolver.Object, sessionObjectSerializer.Object);
+
+        Action act = () => sut.StoreInSession(null!);
+
+        // Act Assert
+        Assert.Throws<ArgumentNullException>(act);
+    }
+
+    [Fact]
+    public void StoreInSession_ResolvesKey_And_Stores_Serialized_Value()
+    {
+        Mock<ISessionObjectKeyResolver> keyResolver = new();
+        keyResolver.Setup(t => t.Resolve<StubSessionObject>()).Returns(It.IsAny<string>());
+
+        Mock<ISession> sessionMock = new();
+        sessionMock
+            .Setup((session) => session.Set(It.IsAny<string>(), It.IsAny<byte[]>()))
+            .Verifiable();
+
+        Mock<IAspNetCoreSessionProvider> sessionProviderMock = new();
+        sessionProviderMock
+            .Setup(provider => provider.GetSession())
+            .Returns(sessionMock.Object)
+            .Verifiable();
+
+        const string stubSerialisedValue = "store_this";
+        Mock<ISessionObjectSerializer<StubSessionObject>> sessionObjectSerializer = new();
+        sessionObjectSerializer
+            .Setup(serializer => serializer.Serialize(It.IsAny<StubSessionObject>()))
+            .Returns(stubSerialisedValue)
+            .Verifiable();
+
+        AspNetCoreSessionCommandHandler<StubSessionObject> sut = new(sessionProviderMock.Object, keyResolver.Object, sessionObjectSerializer.Object);
+
+        StubSessionObject stubSessionObject = new();
+        sut.StoreInSession(stubSessionObject);
+
+        // Act Assert
+        sessionMock.Verify(t => t.Set(It.IsAny<string>(), It.IsAny<byte[]>()), Times.Once);
+        sessionProviderMock.Verify(t => t.GetSession(), Times.Once);
+        keyResolver.Verify(t => t.Resolve<StubSessionObject>(), Times.Once);
+        sessionObjectSerializer.Verify(t => t.Serialize(stubSessionObject), Times.Once);
     }
 
     public sealed class StubSessionObject { }
