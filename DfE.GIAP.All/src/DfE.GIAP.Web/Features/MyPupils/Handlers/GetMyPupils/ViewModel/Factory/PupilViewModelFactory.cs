6092d516a1703f54f01904487a55859db5e01186
aahmed-dfe <Aasim.AHMED@EDUCATION.GOV.UK>
2025-09-02 21:49:19 +0100

    start tests viewmodelfactory

--- DfE.GIAP.All/src/DfE.GIAP.Web/Features/MyPupils/Handlers/GetMyPupils/ViewModel/Factory/PupilViewModelFactory.cs
+++ DfE.GIAP.All/src/DfE.GIAP.Web/Features/MyPupils/Handlers/GetMyPupils/ViewModel/Factory/PupilViewModelFactory.cs
@@ -1,5 +1,6 @@
 ﻿using DfE.GIAP.Core.Common.CrossCutting;
 using DfE.GIAP.Core.MyPupils.Application.UseCases.GetMyPupils.Response;
+using DfE.GIAP.Web.Features.MyPupils.State.Selection;
 
 namespace DfE.GIAP.Web.Features.MyPupils.Handlers.GetMyPupils.ViewModel.Factory;
 
---
+++ DfE.GIAP.All/tests/DfE.GIAP.Web.Tests/Features/MyPupils/GetMyPupils/PupilsViewModelFactoryTests.cs
@@ -0,0 +1,16 @@
+﻿using DfE.GIAP.Web.Features.MyPupils.Handlers.GetMyPupils.ViewModel.Factory;
+using Xunit;
+
+namespace DfE.GIAP.Web.Tests.Features.MyPupils.GetMyPupils;
+public sealed class PupilsViewModelFactoryTests
+{
+    [Fact]
+    public void Constructor_Throws_When_Mapper_Is_Null()
+    {
+        // Arrange
+        Func<PupilViewModelFactory> construct = () => new(null);
+
+        // Act Assert
+        Assert.Throws<ArgumentNullException>(construct);
+    }
+}
