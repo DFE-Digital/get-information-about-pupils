d65310e9db75f47704259cdd1d0824730eeea5a6
Tony Palmer <Tony.PALMER@EDUCATION.GOV.UK>
2025-09-08 11:05:08 +0100

    blob conext tests

---
+++ DfE.GIAP.All/tests/DfE.GIAP.Core.UnitTests/PreparedDownloads/Application/FolderPath/BlobStoragePathContextTests.cs
@@ -0,0 +1,62 @@
+﻿using DfE.GIAP.Core.PreparedDownloads.Application.Enums;
+using DfE.GIAP.Core.PreparedDownloads.Application.FolderPath;
+
+namespace DfE.GIAP.Core.UnitTests.PreparedDownloads.Application.FolderPath;
+public sealed class BlobStoragePathContextTests
+{
+    [Theory]
+    [InlineData(OrganisationScope.Establishment, null, null, "123456", "School/123456/")]
+    [InlineData(OrganisationScope.LocalAuthority, null, "654321", null, "LA/654321/")]
+    [InlineData(OrganisationScope.MultiAcademyTrust, "MAT001", null, null, "MAT/MAT001/")]
+    [InlineData(OrganisationScope.SingleAcademyTrust, "SAT001", null, null, "SAT/SAT001/")]
+    [InlineData(OrganisationScope.AllUsers, null, null, null, "AllUsers/Metadata/")]
+    public void ResolvePath_Returns_ExpectedPath(
+        OrganisationScope scope,
+        string? uniqueIdentifier,
+        string? localAuthorityNumber,
+        string? uniqueReferenceNumber,
+        string expectedPath)
+    {
+        // Arrange
+        BlobStoragePathContext context = BlobStoragePathContext.Create(
+            organisationScope: scope,
+            uniqueIdentifier: uniqueIdentifier,
+            localAuthorityNumber: localAuthorityNumber,
+            uniqueReferenceNumber: uniqueReferenceNumber);
+
+        // Act
+        string actualPath = context.ResolvePath();
+
+        // Assert
+        Assert.Equal(expectedPath, actualPath);
+    }
+
+    [Theory]
+    [InlineData(OrganisationScope.Establishment)]
+    [InlineData(OrganisationScope.LocalAuthority)]
+    [InlineData(OrganisationScope.MultiAcademyTrust)]
+    [InlineData(OrganisationScope.SingleAcademyTrust)]
+    public void Create_Throws_ArgumentNullException_WhenRequiredFieldIsMissing(OrganisationScope scope)
+    {
+        // Act & Assert
+        ArgumentException ex = Assert.Throws<ArgumentNullException>(() =>
+        {
+            BlobStoragePathContext.Create(scope);
+        });
+    }
+
+    [Fact]
+    public void Create_ThrowsNotImplementedException_ForUnhandledScope()
+    {
+        // Arrange
+        OrganisationScope invalidScope = (OrganisationScope)999;
+
+        // Act & Assert
+        NotImplementedException ex = Assert.Throws<NotImplementedException>(() =>
+        {
+            BlobStoragePathContext.Create(invalidScope);
+        });
+
+        Assert.Contains("Unhandled OrganisationScope", ex.Message);
+    }
+}
