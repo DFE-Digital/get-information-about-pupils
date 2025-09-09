2457390b86d835b578c897d62c5fbc56763ea81a
TonyPalmerDfE <tony.palmer@education.gov.uk>
2025-09-09 12:46:18 +0100

    refactor: remove unused prepared code (#284)

--- DfE.GIAP.All/src/DfE.GIAP.Service/ApiProcessor/IApiService.cs
+++ DfE.GIAP.All/src/DfE.GIAP.Service/ApiProcessor/IApiService.cs
@@ -1,27 +1,20 @@
 ﻿using DfE.GIAP.Domain.Models.Common;
-using System;
-using System.Collections.Generic;
 using System.Net;
-using System.Net.Http;
-using System.Threading.Tasks;
 
-namespace DfE.GIAP.Service.ApiProcessor
+namespace DfE.GIAP.Service.ApiProcessor;
+
+public interface IApiService
 {
-    public interface IApiService
-    {
-        Task<TApiModel> GetAsync<TApiModel>(Uri url)
-            where TApiModel : class;
+    Task<TApiModel> GetAsync<TApiModel>(Uri url)
+        where TApiModel : class;
 
-        Task<List<TApiModel>> GetToListAsync<TApiModel>(Uri url)
-            where TApiModel : class;
+    Task<List<TApiModel>> GetToListAsync<TApiModel>(Uri url)
+        where TApiModel : class;
 
-        Task<HttpStatusCode> PostAsync<TModel>(Uri url, TModel model)
-            where TModel : class;
+    Task<HttpStatusCode> PostAsync<TModel>(Uri url, TModel model)
+        where TModel : class;
 
-        Task<TResponseModel> PostAsync<TRequestModel, TResponseModel>(Uri url, TRequestModel model, AzureFunctionHeaderDetails headerDetails)
-            where TRequestModel : class
-            where TResponseModel : class;
-
-        Task<HttpStatusCode> DeleteAsync(Uri url);
-    }
+    Task<TResponseModel> PostAsync<TRequestModel, TResponseModel>(Uri url, TRequestModel model, AzureFunctionHeaderDetails headerDetails)
+        where TRequestModel : class
+        where TResponseModel : class;
 }
