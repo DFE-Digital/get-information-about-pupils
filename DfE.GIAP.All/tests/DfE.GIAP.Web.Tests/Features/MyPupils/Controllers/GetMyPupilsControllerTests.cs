fa8b88c11bfbe242528b58be0c41b7e2d93839ed
aahmed-dfe <Aasim.AHMED@EDUCATION.GOV.UK>
2025-09-17 10:18:35 +0100

    controller test for null guards wip

--- DfE.GIAP.All/tests/DfE.GIAP.Web.Tests/Features/MyPupils/Controllers/GetMyPupilsControllerTests.cs
+++ DfE.GIAP.All/tests/DfE.GIAP.Web.Tests/Features/MyPupils/Controllers/GetMyPupilsControllerTests.cs
@@ -1,4 +1,9 @@
-﻿using Xunit;
+﻿using DfE.GIAP.Web.Features.MyPupils.Routes;
+using DfE.GIAP.Web.Features.MyPupils.Services.GetMyPupilsForUser;
+using DfE.GIAP.Web.Features.MyPupils.State;
+using DfE.GIAP.Web.Features.MyPupils.ViewModel;
+using Moq;
+using Xunit;
 
 namespace DfE.GIAP.Web.Tests.Features.MyPupils.Controllers;
 public sealed class GetMyPupilsControllerTests
@@ -10,7 +15,58 @@ public sealed class GetMyPupilsControllerTests
 
         // Act
 
+        GetMyPupilsController sut = new(null, null, null, null);
+        sut.StubHttpContext();
+
         // Assert
         Assert.True(true);
     }
+
+    [Fact]
+    public void Constructor_Throws_When_Logger_Is_Null()
+    {
+        // Arrange
+        Mock<IGetMyPupilsStateProvider> stateProviderMock = new();
+        Mock<IGetPupilViewModelsHandler> handlerMock = new();
+        Mock<IMyPupilsViewModelFactory> viewModelFactoryMock = new();
+        // Act Assert
+        Func<GetMyPupilsController> construct = () => new(
+            logger: null,
+            viewModelFactoryMock.Object,
+            stateProviderMock.Object,
+            handlerMock.Object);
+
+        Assert.Throws<ArgumentNullException>(construct);
+    }
+
+    [Fact]
+    public void Constructor_Throws_When_StateProvider_Is_Null()
+    {
+        // Arrange
+
+        // Act Assert
+        Func<GetMyPupilsController> construct = () => new(null, null, null, null);
+        Assert.Throws<ArgumentNullException>(construct);
+    }
+
+    [Fact]
+    public void Constructor_Throws_When_ViewModelFactory_Is_Null()
+    {
+        // Arrange
+
+        // Act Assert
+        Func<GetMyPupilsController> construct = () => new(null, null, null, null);
+        Assert.Throws<ArgumentNullException>(construct);
+    }
+
+    [Fact]
+    public void Constructor_Throws_When_Handler_Is_Null()
+    {
+        // Arrange
+
+        // Act Assert
+        Func<GetMyPupilsController> construct = () => new(null, null, null, null);
+        Assert.Throws<ArgumentNullException>(construct);
+    }
+
 }
