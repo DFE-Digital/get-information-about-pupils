e9eb3e4be0fd356c9353f4a797560a55556f00fa
aahmed-dfe <Aasim.AHMED@EDUCATION.GOV.UK>
2025-10-04 14:33:52 +0100

    adjust script to variables, debuggin to single quotes

--- scripts/run_integration_tests.sh
+++ scripts/run_integration_tests.sh
@@ -1,32 +1,38 @@
 #!/bin/bash
 set -e
 
-# Resolve absolute path to the directory containing this script
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+# CosmosDb certificate
 
 retry_count=0
 max_retry_count=50
 
-until curl --insecure --silent --fail "https://localhost:8081/_explorer/emulator.pem"; do
+COSMOSDB_CERT_URL='https://localhost:8081/_explorer/emulator.pem'
+
+until curl --insecure --silent --fail "$COSMOSDB_CERT_URL"; do
   if [ $retry_count -eq $max_retry_count ]; then
     echo "Cosmos DB Emulator did not become healthy in time."
     exit 1
   fi
   echo "Waiting for Cosmos DB Emulator... ($retry_count)"
-  sleep 5
+  sleep 3
   retry_count=$((retry_count+1))
 done
 
 echo "Cosmos DB Emulator is healthy ..."
 
-echo "Getting certificate ..."
+echo 'Outputting certificate to host certificate store ...'
 
-curl -k https://localhost:8081/_explorer/emulator.pem -o /usr/local/share/ca-certificates/cosmos-db-emulator.crt
+curl -k "$COSMOSDB_CERT_URL" -o /usr/local/share/ca-certificates/cosmos-db-emulator.crt
+
+
+# Resolve absolute path to the directory containing this script
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 ## WireMock certificate
 
-chmod +x "$SCRIPT_DIR/generate_wiremock_server_cert.sh"
-"$SCRIPT_DIR/generate_wiremock_server_cert.sh"
+WIREMOCK_SCRIPT_PATH="$SCRIPT_DIR/generate_wiremock_server_cert.sh"
+chmod +x "$WIREMOCK_SCRIPT_PATH" && /bin/bash -c $WIREMOCK_SCRIPT_PATH
 
 update-ca-certificates
 
