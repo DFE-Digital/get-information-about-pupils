1f79960cd1e39c493814ad24d7b25d75790dc57f
aahmed-dfe <Aasim.AHMED@EDUCATION.GOV.UK>
2025-10-07 13:15:56 +0100

    fix docker compose, mount in the secret using docker buildkit so its not cached in the image layers

--- scripts/generate_wiremock_server_cert.sh
+++ scripts/generate_wiremock_server_cert.sh
@@ -1,9 +1,8 @@
-#!/bin/bash
-
-# Generate a local CA (certificate authority)
+#!/bin/sh
 
 set -e
 
+# Generate a local CA (certificate authority)
 echo '[req]
 distinguished_name = req_distinguished_name
 x509_extensions = v3_ca
@@ -19,7 +18,6 @@ openssl req -x509 -new -nodes -key ca.key -sha256 -days 3650 -out ca.crt -subj "
 openssl genrsa -out wiremock-cert.key 2048
 
 # Generate CSR (Certificate signing request) to sign WireMock self-generated certificate with local CA
-
 openssl req -new -key wiremock-cert.key -out wiremock-cert.csr -subj '/CN=localhost'
 
 echo 'authorityKeyIdentifier=keyid,issuer
@@ -38,10 +36,6 @@ openssl x509 -req -in wiremock-cert.csr -CA ca.crt -CAkey ca.key -CAcreateserial
 openssl pkcs12 -export -out wiremock-cert.pfx -inkey wiremock-cert.key -in wiremock-cert.crt -certfile ca.crt -passout pass:yourpassword
 
 # Move certificate into directory where integration tests use
-
-echo 'Printing current directory...'
-ls -la .
-
 echo 'Moving signed certificated localhost.pfx into Integration tests output directory...'
 mv wiremock-cert.pfx DfE.GIAP.All/tests/DfE.GIAP.Core.IntegrationTests/bin/Debug/net8.0
 echo 'Moved signed certificate'
