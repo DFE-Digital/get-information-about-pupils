47216e9cb5d53e67c71f870d2a82b54fdb098ca5
aahmed-dfe <Aasim.AHMED@EDUCATION.GOV.UK>
2025-10-23 08:54:51 +0100

    revert removal of cosmosdb deps

--- DfE.GIAP.All/src/DfE.GIAP.Core/Common/CompositionRoot.cs
+++ DfE.GIAP.All/src/DfE.GIAP.Core/Common/CompositionRoot.cs
@@ -1,4 +1,5 @@
-﻿using DfE.GIAP.Core.Common.Application.TextSanitiser.Handlers;
+﻿using DfE.Data.ComponentLibrary.Infrastructure.Persistence.CosmosDb;
+using DfE.GIAP.Core.Common.Application.TextSanitiser.Handlers;
 using DfE.GIAP.Core.Common.Application.TextSanitiser.Invoker;
 using DfE.GIAP.Core.Common.CrossCutting.Logging;
 using Microsoft.Extensions.DependencyInjection;
@@ -10,6 +11,7 @@ public static class CompositionRoot
     public static IServiceCollection AddFeaturesSharedDependencies(this IServiceCollection services)
     {
         ArgumentNullException.ThrowIfNull(services);
+        services.AddCosmosDbDependencies();
         services.TryAddEnumerable(new ServiceDescriptor(typeof(IEnumerable<ITextSanitiserHandler>), Array.Empty<ITextSanitiserHandler>()));
         services.AddSingleton<ITextSanitiserInvoker, TextSanitisationInvoker>();
 
