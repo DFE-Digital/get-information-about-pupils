ba16b188ec81be2906cec70b83007bb02becb60b
aahmed-dfe <119260940+aahmed-dfe@users.noreply.github.com>
2025-10-23 09:08:33 +0100

    tests: Update to ConfigurationTestDoubles (#359)
    * udpates to config builder
    
    * format
    
    * remove duplicate reg

---
+++ DfE.GIAP.All/tests/DfE.GIAP.SharedTests/TestDoubles/Configuration/IConfigurationBuilderExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Memory;
+
+namespace DfE.GIAP.SharedTests.TestDoubles.Configuration;
+internal static class IConfigurationBuilderExtensions
+{
+    internal static void AddConfiguration(this IConfigurationBuilder builder, IEnumerable<KeyValuePair<string, string?>> config)
+    {
+        ArgumentNullException.ThrowIfNull(config);
+
+        builder.Add(
+            new MemoryConfigurationSource()
+            {
+                InitialData = config
+            });
+    }
+}
