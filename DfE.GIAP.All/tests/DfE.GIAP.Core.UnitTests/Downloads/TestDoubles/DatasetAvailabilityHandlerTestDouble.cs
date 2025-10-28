3893fe90ff14ffe7b096627782d7343f4f8e459c
Tony Palmer <Tony.PALMER@EDUCATION.GOV.UK>
2025-10-28 12:29:36 +0000

    availability handlers and factory tests added

---
+++ DfE.GIAP.All/tests/DfE.GIAP.Core.UnitTests/Downloads/TestDoubles/DatasetAvailabilityHandlerTestDouble.cs
@@ -0,0 +1,24 @@
+﻿using DfE.GIAP.Core.Downloads.Application.Datasets.Availability.Handlers;
+using DfE.GIAP.Core.Downloads.Application.Enums;
+
+namespace DfE.GIAP.Core.UnitTests.Downloads.TestDoubles;
+
+public static class DatasetAvailabilityHandlerTestDouble
+{
+    public static IDatasetAvailabilityHandler Create(DownloadType type) => new StubHandler(type);
+
+    private sealed class StubHandler : IDatasetAvailabilityHandler
+    {
+        public DownloadType SupportedDownloadType { get; }
+
+        public StubHandler(DownloadType type)
+        {
+            SupportedDownloadType = type;
+        }
+
+        public Task<IEnumerable<Dataset>> GetAvailableDatasetsAsync(IEnumerable<string> pupilIds)
+        {
+            return Task.FromResult<IEnumerable<Dataset>>(new[] { Dataset.PP });
+        }
+    }
+}
---
+++ DfE.GIAP.All/tests/DfE.GIAP.Core.UnitTests/Downloads/TestDoubles/FurtherEducationRepositoryTestDouble.cs
@@ -0,0 +1,25 @@
+﻿using DfE.GIAP.Core.Downloads.Application.Models;
+using DfE.GIAP.Core.Downloads.Application.Repositories;
+
+namespace DfE.GIAP.Core.UnitTests.Downloads.TestDoubles;
+
+public static class FurtherEducationRepositoryTestDouble
+{
+    public static IFurtherEducationRepository WithPupils(IEnumerable<FurtherEducationPupil> pupils) =>
+        new StubRepository(pupils);
+
+    private sealed class StubRepository : IFurtherEducationRepository
+    {
+        private readonly IEnumerable<FurtherEducationPupil> _pupils;
+
+        public StubRepository(IEnumerable<FurtherEducationPupil> pupils)
+        {
+            _pupils = pupils;
+        }
+
+        public Task<IEnumerable<FurtherEducationPupil>> GetPupilsByIdsAsync(IEnumerable<string> pupilIds)
+        {
+            return Task.FromResult(_pupils);
+        }
+    }
+}
