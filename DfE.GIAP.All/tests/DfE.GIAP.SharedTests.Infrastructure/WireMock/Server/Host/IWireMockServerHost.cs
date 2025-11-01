62f26ea294683c0bef8e99b5ffd74815fecb9221
aahmed-dfe <Aasim.AHMED@EDUCATION.GOV.UK>
2025-11-01 21:41:37 +0000

    separate server host, and client creation - srp

---
+++ DfE.GIAP.All/tests/DfE.GIAP.SharedTests.Infrastructure/WireMock/Server/Host/IWireMockServerHost.cs
@@ -0,0 +1,10 @@
+﻿using DfE.GIAP.SharedTests.Infrastructure.WireMock.Server.Client;
+
+namespace DfE.GIAP.SharedTests.Infrastructure.WireMock.Server.Host;
+// TODO AsyncDisposable
+public interface IWireMockServerHost : IDisposable
+{
+    Uri Endpoint { get; }
+    Task StartAsync();
+    IWireMockClient CreateClient();
+}
