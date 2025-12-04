64b959ed46296feb4dbaddd6c4cac8d828dda3f4
Tony Palmer <Tony.PALMER@EDUCATION.GOV.UK>
2025-12-04 13:13:03 +0000

    adjust event param names for logging

--- DfE.GIAP.All/src/DfE.GIAP.Core/Common/CrossCutting/Logging/Events/Models/BusinessEvent.cs
+++ DfE.GIAP.All/src/DfE.GIAP.Core/Common/CrossCutting/Logging/Events/Models/BusinessEvent.cs
@@ -28,8 +28,8 @@ public abstract record BusinessEvent(
 
     protected IDictionary<string, string> BaseProperties() => new Dictionary<string, string>
     {
-        ["UserId"] = UserId,
-        ["SessionId"] = SessionId,
+        ["UserID"] = UserId,
+        ["SessionID"] = SessionId,
         ["Description"] = Description,
         ["OrgURN"] = OrgURN,
         ["OrgName"] = OrgName,
