73fad3bbc619b969c807382d27dbf7a612814a1e
aahmed-dfe <Aasim.AHMED@EDUCATION.GOV.UK>
2026-01-07 10:52:45 +0000

    fulfil ctor null guards

--- DfE.GIAP.All/tests/DfE.GIAP.Web.Tests/Features/MyPupils/Messaging/MyPupilsTempDataMessageSinkTests.cs
+++ DfE.GIAP.All/tests/DfE.GIAP.Web.Tests/Features/MyPupils/Messaging/MyPupilsTempDataMessageSinkTests.cs
@@ -9,7 +9,6 @@ using Xunit;
 namespace DfE.GIAP.Web.Tests.Features.MyPupils.Messaging;
 public sealed class MyPupilsTempDataMessageSinkTests
 {
-
     [Fact]
     public void Constructor_Throws_When_ToDataTransferObjectMapper_Null()
     {
@@ -19,6 +18,62 @@ public sealed class MyPupilsTempDataMessageSinkTests
             MapperTestDoubles.Default<MyPupilsMessageDto, MyPupilsMessage>().Object,
             OptionsTestDoubles.Default<MyPupilsMessagingOptions>(),
             new Mock<ITempDataDictionaryProvider>().Object);
+
+        // Act Assert
+        Assert.Throws<ArgumentNullException>(construct);
+    }
+
+    [Fact]
+    public void Constructor_Throws_When_FromDataTransferObjectMapper_Null()
+    {
+        // Arrange
+        Func<MyPupilsTempDataMessageSink> construct = () => new(
+            MapperTestDoubles.Default<MyPupilsMessage, MyPupilsMessageDto>().Object,
+            null!,
+            OptionsTestDoubles.Default<MyPupilsMessagingOptions>(),
+            new Mock<ITempDataDictionaryProvider>().Object);
+
+        // Act Assert
+        Assert.Throws<ArgumentNullException>(construct);
+    }
+
+    [Fact]
+    public void Constructor_Throws_When_Options_Null()
+    {
+        // Arrange
+        Func<MyPupilsTempDataMessageSink> construct = () => new(
+            MapperTestDoubles.Default<MyPupilsMessage, MyPupilsMessageDto>().Object,
+            MapperTestDoubles.Default<MyPupilsMessageDto, MyPupilsMessage>().Object,
+            null!,
+            new Mock<ITempDataDictionaryProvider>().Object);
+
+        // Act Assert
+        Assert.Throws<ArgumentNullException>(construct);
+    }
+
+    [Fact]
+    public void Constructor_Throws_When_OptionsValue_Null()
+    {
+        // Arrange
+        Func<MyPupilsTempDataMessageSink> construct = () => new(
+            MapperTestDoubles.Default<MyPupilsMessage, MyPupilsMessageDto>().Object,
+            MapperTestDoubles.Default<MyPupilsMessageDto, MyPupilsMessage>().Object,
+            OptionsTestDoubles.MockNullOptions<MyPupilsMessagingOptions>(),
+            new Mock<ITempDataDictionaryProvider>().Object);
+
+        // Act Assert
+        Assert.Throws<ArgumentNullException>(construct);
+    }
+
+    [Fact]
+    public void Constructor_Throws_When_TempDataProvider_Null()
+    {
+        // Arrange
+        Func<MyPupilsTempDataMessageSink> construct = () => new(
+            MapperTestDoubles.Default<MyPupilsMessage, MyPupilsMessageDto>().Object,
+            MapperTestDoubles.Default<MyPupilsMessageDto, MyPupilsMessage>().Object,
+            OptionsTestDoubles.Default<MyPupilsMessagingOptions>(),
+            null!);
 
         // Act Assert
         Assert.Throws<ArgumentNullException>(construct);
