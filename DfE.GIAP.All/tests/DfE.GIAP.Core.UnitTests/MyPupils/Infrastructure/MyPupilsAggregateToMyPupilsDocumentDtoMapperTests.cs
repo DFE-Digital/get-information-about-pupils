71aef26623b0437ed386289b1180d647a2ca827e
aahmed-dfe <Aasim.AHMED@EDUCATION.GOV.UK>
2026-01-08 15:58:10 +0000

    start documentdto mapper - map throws null

---
+++ DfE.GIAP.All/tests/DfE.GIAP.Core.UnitTests/MyPupils/Infrastructure/MyPupilsAggregateToMyPupilsDocumentDtoMapperTests.cs
@@ -0,0 +1,18 @@
+﻿using DfE.GIAP.Core.MyPupils.Infrastructure.Repositories.DataTransferObjects;
+using DfE.GIAP.Core.MyPupils.Infrastructure.Repositories.Write;
+
+namespace DfE.GIAP.Core.UnitTests.MyPupils.Infrastructure;
+public sealed class MyPupilsAggregateToMyPupilsDocumentDtoMapperTests
+{
+    [Fact]
+    public void Map_Null_Throws_ArgumentNullException()
+    {
+        // Arrange
+        MyPupilsAggregateToMyPupilsDocumentDtoMapper sut = new();
+
+        Func<MyPupilsDocumentDto> act = () => sut.Map(null!);
+
+        // Act & Assert
+        Assert.Throws<ArgumentNullException>(act);
+    }
+}
