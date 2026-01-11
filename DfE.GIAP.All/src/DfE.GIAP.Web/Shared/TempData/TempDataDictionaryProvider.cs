db6fa1e2e9c270b58a609df48dee259339cb6ac7
aahmed-dfe <119260940+aahmed-dfe@users.noreply.github.com>
2026-01-11 13:49:25 +0000

    temp data provider and tests (#468)

---
+++ DfE.GIAP.All/src/DfE.GIAP.Web/Shared/TempData/TempDataDictionaryProvider.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc.ViewFeatures;
+
+namespace DfE.GIAP.Web.Shared.TempData;
+
+public sealed class TempDataDictionaryProvider : ITempDataDictionaryProvider
+{
+    private readonly IHttpContextAccessor _httpContextAccessor;
+    private readonly ITempDataDictionaryFactory _tempDataDictionaryFactory;
+
+    public TempDataDictionaryProvider(
+        IHttpContextAccessor httpContextAccessor,
+        ITempDataDictionaryFactory tempDataDictionaryFactory)
+    {
+        ArgumentNullException.ThrowIfNull(httpContextAccessor);
+        _httpContextAccessor = httpContextAccessor;
+
+        ArgumentNullException.ThrowIfNull(tempDataDictionaryFactory);
+        _tempDataDictionaryFactory = tempDataDictionaryFactory;
+    }
+    public ITempDataDictionary GetTempData()
+    {
+        HttpContext context = _httpContextAccessor.HttpContext;
+        ArgumentNullException.ThrowIfNull(context);
+        return _tempDataDictionaryFactory.GetTempData(context);
+    }
+}
