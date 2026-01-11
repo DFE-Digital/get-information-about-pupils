ae05d3cde994d7b1bb0aecfbf1ede0496b01073c
aahmed-dfe <Aasim.AHMED@EDUCATION.GOV.UK>
2026-01-11 22:23:26 +0000

    add mapper tests

---
+++ DfE.GIAP.All/tests/DfE.GIAP.Web.Tests/Features/MyPupils/Messaging/MyPupilsMessageDtoToMyPupilsMessageMapperTests.cs
@@ -0,0 +1,42 @@
+﻿using DfE.GIAP.Web.Features.MyPupils.Messaging;
+using DfE.GIAP.Web.Features.MyPupils.Messaging.DataTransferObjects;
+using DfE.GIAP.Web.Features.MyPupils.Messaging.Mapper;
+using Xunit;
+
+namespace DfE.GIAP.Web.Tests.Features.MyPupils.Messaging;
+public sealed class MyPupilsMessageDtoToMyPupilsMessageMapperTests
+{
+    [Fact]
+    public void Handle_Throws_When_Input_Is_Null()
+    {
+        // Arrange
+        MyPupilsMessageDtoToMyPupilsMessageMapper sut = new();
+
+        // Act
+        Func<MyPupilsMessage> act = () => sut.Map(null!);
+
+        // Assert
+        Assert.Throws<ArgumentNullException>(act);
+    }
+
+    [Fact]
+    public void Map_Maps_MessageDto_To_Message()
+    {
+        // Arrange
+        MyPupilsMessageDto input = new()
+        {
+            Id = "id",
+            MessageLevel = MessageLevel.Info,
+            Message = "Test message"
+        };
+
+        MyPupilsMessageDtoToMyPupilsMessageMapper sut = new();
+
+        // Act
+        MyPupilsMessage response = sut.Map(input);
+
+        Assert.Equal(input.Id, response.Id);
+        Assert.Equal(input.Message, response.Message);
+        Assert.Equal(input.MessageLevel, response.Level);
+    }
+}
