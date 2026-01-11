a87b958b66ad16c4ecd184c7381a7ec56e61e35f
aahmed-dfe <Aasim.AHMED@EDUCATION.GOV.UK>
2026-01-11 15:21:21 +0000

    add command handlers that apply state selection predicated on request

---
+++ DfE.GIAP.All/tests/DfE.GIAP.Web.Tests/Features/MyPupils/PupilSelection/UpdatePupilSelections/Handlers/ManualSelectPupilsCommandHandler.cs
@@ -0,0 +1,22 @@
+﻿using DfE.GIAP.Core.Common.CrossCutting.ChainOfResponsibility.CommandHandler;
+using DfE.GIAP.Web.Features.MyPupils.PupilSelection.UpdatePupilSelections;
+
+namespace DfE.GIAP.Web.Tests.Features.MyPupils.PupilSelection.UpdatePupilSelections.Handlers;
+internal sealed class ManualSelectPupilsCommandHandler : ICommandHandler<UpdateMyPupilsSelectionStateRequest>
+{
+    public bool CanHandle(UpdateMyPupilsSelectionStateRequest input) => true;
+    public void Handle(UpdateMyPupilsSelectionStateRequest input)
+    {
+        List<string> selectedPupilsOnPage = input.UpdateRequest.SelectedPupils?.ToList() ?? [];
+        List<string> deselectedPupilsOnPage = input.UpdateRequest.CurrentPupils.Except(selectedPupilsOnPage).ToList();
+
+        foreach (string upn in deselectedPupilsOnPage)
+        {
+            input.State.Deselect(upn);
+        }
+        foreach (string upn in selectedPupilsOnPage)
+        {
+            input.State.Select(upn);
+        }
+    }
+}
