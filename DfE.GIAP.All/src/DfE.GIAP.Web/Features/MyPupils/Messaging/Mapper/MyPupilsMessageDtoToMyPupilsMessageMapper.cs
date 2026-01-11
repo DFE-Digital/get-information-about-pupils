f9020dabd76e0d5645b7f0941cc1eede21d75dda
aahmed-dfe <Aasim.AHMED@EDUCATION.GOV.UK>
2026-01-11 21:20:31 +0000

    add messagign impl around tempdata

---
+++ DfE.GIAP.All/src/DfE.GIAP.Web/Features/MyPupils/Messaging/Mapper/MyPupilsMessageDtoToMyPupilsMessageMapper.cs
@@ -0,0 +1,15 @@
+﻿using DfE.GIAP.Web.Features.MyPupils.Messaging.DataTransferObjects;
+
+namespace DfE.GIAP.Web.Features.MyPupils.Messaging.Mapper;
+
+public sealed class MyPupilsMessageDtoToMyPupilsMessageMapper : IMapper<MyPupilsMessageDto, MyPupilsMessage>
+{
+    public MyPupilsMessage Map(MyPupilsMessageDto input)
+    {
+        ArgumentNullException.ThrowIfNull(input);
+        return new(
+            id: input.Id,
+            level: input.MessageLevel,
+            message: input.Message);
+    }
+}
