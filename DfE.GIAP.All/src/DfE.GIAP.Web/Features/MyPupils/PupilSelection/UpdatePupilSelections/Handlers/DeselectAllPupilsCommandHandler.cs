50d4fd3a704ba8a0f90426697c57b570eb80da39
aahmed-dfe <Aasim.AHMED@EDUCATION.GOV.UK>
2026-01-11 15:27:22 +0000

    add update selectionState command handlers

---
+++ DfE.GIAP.All/src/DfE.GIAP.Web/Features/MyPupils/PupilSelection/UpdatePupilSelections/Handlers/DeselectAllPupilsCommandHandler.cs
@@ -0,0 +1,16 @@
+﻿using DfE.GIAP.Core.Common.CrossCutting.ChainOfResponsibility.CommandHandler;
+using DfE.GIAP.Web.Features.MyPupils.Controllers.UpdateForm;
+
+namespace DfE.GIAP.Web.Features.MyPupils.PupilSelection.UpdatePupilSelections.Handlers;
+
+internal sealed class DeselectAllPupilsCommandHandler : ICommandHandler<UpdateMyPupilsSelectionStateRequest>
+{
+    public bool CanHandle(UpdateMyPupilsSelectionStateRequest input)
+        => input.UpdateRequest.SelectAllState == MyPupilsFormSelectionModeRequestDto.DeselectAll;
+
+    public void Handle(UpdateMyPupilsSelectionStateRequest input)
+    {
+        ArgumentNullException.ThrowIfNull(input);
+        input.State.DeselectAll();
+    }
+}
