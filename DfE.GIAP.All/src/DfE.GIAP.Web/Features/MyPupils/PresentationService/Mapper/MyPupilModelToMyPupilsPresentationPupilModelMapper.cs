892d50717c4c50e2bea6d8e29d10529af4c41d25
aahmed-dfe <119260940+aahmed-dfe@users.noreply.github.com>
2026-01-11 13:43:18 +0000

    add mappers and tests (#467)

---
+++ DfE.GIAP.All/src/DfE.GIAP.Web/Features/MyPupils/PresentationService/Mapper/MyPupilModelToMyPupilsPresentationPupilModelMapper.cs
@@ -0,0 +1,23 @@
+﻿using DfE.GIAP.Core.Common.CrossCutting;
+using DfE.GIAP.Core.MyPupils.Application.UseCases.GetMyPupils;
+
+namespace DfE.GIAP.Web.Features.MyPupils.PresentationService.Mapper;
+
+internal sealed class MyPupilModelToMyPupilsPresentationPupilModelMapper : IMapper<MyPupilsModel, MyPupilsPresentationPupilModel>
+{
+    public MyPupilsPresentationPupilModel Map(MyPupilsModel input)
+    {
+        ArgumentNullException.ThrowIfNull(input);
+
+        return new MyPupilsPresentationPupilModel()
+        {
+            UniquePupilNumber = input.UniquePupilNumber,
+            Forename = input.Forename,
+            Surname = input.Surname,
+            DateOfBirth = input.DateOfBirth,
+            PupilPremiumLabel = input.IsPupilPremium ? "Yes" : "No",
+            Sex = input.Sex,
+            LocalAuthorityCode = input.LocalAuthorityCode.ToString()
+        };
+    }
+}
