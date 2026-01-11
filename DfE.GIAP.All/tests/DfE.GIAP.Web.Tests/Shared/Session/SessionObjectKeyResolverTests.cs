02301014a4e44fe5f3a4e1161a3cf52a9a99f417
aahmed-dfe <Aasim.AHMED@EDUCATION.GOV.UK>
2026-01-11 22:50:17 +0000

    fix keys following namespace change

--- DfE.GIAP.All/tests/DfE.GIAP.Web.Tests/Shared/Session/SessionObjectKeyResolverTests.cs
+++ DfE.GIAP.All/tests/DfE.GIAP.Web.Tests/Shared/Session/SessionObjectKeyResolverTests.cs
@@ -1,17 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using DfE.GIAP.Web.Shared.Session.Infrastructure;
+﻿using DfE.GIAP.Web.Shared.Session.Infrastructure;
 using Xunit;
 
 namespace DfE.GIAP.Web.Tests.Shared.Session;
 public sealed class SessionObjectKeyResolverTests
 {
     [Theory]
-    [InlineData(typeof(NestedSessionObject), "DfE.GIAP.Web.Tests.Session.SessionObjectKeyResolverTests+NestedSessionObject")]
-    [InlineData(typeof(SessionObject), "DfE.GIAP.Web.Tests.Session.SessionObject")]
+    [InlineData(typeof(NestedSessionObject), "DfE.GIAP.Web.Tests.Shared.Session.SessionObjectKeyResolverTests+NestedSessionObject")]
+    [InlineData(typeof(SessionObject), "DfE.GIAP.Web.Tests.Shared.Session.SessionObject")]
     public void Resolves_Type_Returns_SessionKey_To_NamespaceAndType(Type type, string expectedKey)
     {
         SessionObjectKeyResolver sut = new();
@@ -25,7 +20,7 @@ public sealed class SessionObjectKeyResolverTests
     [Fact]
     public void Resolves_GenericType_Returns_SessionKey_To_NamespaceAndType()
     {
-        string expectedKey = "DfE.GIAP.Web.Tests.Session.SessionObject";
+        string expectedKey = "DfE.GIAP.Web.Tests.Shared.Session.SessionObject";
 
         SessionObjectKeyResolver sut = new();
 
