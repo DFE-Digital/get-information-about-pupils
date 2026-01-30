b40af76f6e7db0948a1389c3b835146f6de8167c
aahmed-dfe <Aasim.AHMED@EDUCATION.GOV.UK>
2026-01-30 09:42:09 +0000

    mapper tests dto to read model

---
+++ DfE.GIAP.All/tests/DfE.GIAP.Core.UnitTests/Search/Infrastructure/TestDoubles/NationalPupilDatabaseLearnerDataTransferObjectTestDoubles.cs
@@ -0,0 +1,33 @@
+﻿using Bogus;
+using DfE.GIAP.Core.Search.Application.Models.Learner;
+using DfE.GIAP.Core.Search.Infrastructure.NationalPupilDatabase.DataTransferObjects;
+using DfE.GIAP.SharedTests.Common;
+using DfE.GIAP.SharedTests.TestDoubles;
+using DfE.GIAP.SharedTests.TestDoubles.Learner;
+
+namespace DfE.GIAP.Core.UnitTests.Search.Infrastructure.TestDoubles;
+public static class NationalPupilDatabaseLearnerDataTransferObjectTestDoubles
+{
+    public static NationalPupilDatabaseLearnerDataTransferObject Stub()
+    {
+        Faker faker = new();
+
+        string gender = faker
+            .PickRandom(Gender.Male, Gender.Female, Gender.Other)
+            .MapSexDescription();
+
+        NationalPupilDatabaseLearnerDataTransferObject dto = new()
+        {
+            UPN = UniquePupilNumberTestDoubles.Generate().Value,
+            Forename = faker.Name.FirstName(),
+            Middlenames = faker.Name.FirstName(),
+            Surname = faker.Name.LastName(),
+            DOB = DateTimeTestDoubles.GenerateDateOfBirthForAgeOf(10),
+            LocalAuthority = LocalAuthorityCodeTestDoubles.Stub().Code.ToString(),
+            Sex = gender,
+            Gender = gender
+        };
+
+        return dto;
+    }
+}
