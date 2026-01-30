23f3b0d78a4869a3938a1de49cd9f8e7bdf92a3c
aahmed-dfe <Aasim.AHMED@EDUCATION.GOV.UK>
2026-01-30 10:13:20 +0000

    add numeric mapping context tests

---
+++ DfE.GIAP.All/tests/DfE.GIAP.Web.Tests/Features/Search/NationalPupilDatabase/SearchByUniquePupilNumber/NationalPupilDatabaseLearnerNumericSearchMappingContextToViewModelMapperTests.cs
@@ -0,0 +1,70 @@
+﻿using DfE.GIAP.Core.Search.Application.Models.Search;
+using DfE.GIAP.Core.Search.Application.UseCases.NationalPupilDatabase;
+using DfE.GIAP.Core.Search.Application.UseCases.NationalPupilDatabase.Models;
+using DfE.GIAP.Domain.Search.Learner;
+using DfE.GIAP.SharedTests.TestDoubles.Learner;
+using DfE.GIAP.Web.Features.Search.NationalPupilDatabase.SearchByUniquePupilNumber;
+using DfE.GIAP.Web.Tests.Features.Search.NationalPupilDatabase.TestDoubles;
+using DfE.GIAP.Web.Tests.Features.Search.PupilPremium.TestDoubles;
+using DfE.GIAP.Web.Tests.Features.Search.Shared.TestDoubles;
+using DfE.GIAP.Web.ViewModels.Search;
+
+namespace DfE.GIAP.Web.Tests.Features.Search.NationalPupilDatabase.SearchByUniquePupilNumber;
+public sealed class NationalPupilDatabaseLearnerNumericSearchMappingContextToViewModelMapperTests
+{
+
+    [Fact]
+    public void Constructor_WithNullLearnerMapper_ThrowsArgumentNullException()
+    {
+        // act/assert
+        Assert.Throws<ArgumentNullException>(() =>
+            new NationalPupilDatabaseLearnerNumericSearchMappingContextToViewModelMapper(null!));
+    }
+
+    [Fact]
+    public void Map_WithValidContext_MapsLearnersAndFiltersCorrectly()
+    {
+        // arrange
+        List<Learner> domainLearners = [
+            LearnerFakePupilPremiumTestDoubles.Fake(),
+            LearnerFakePupilPremiumTestDoubles.Fake()
+        ];
+
+        List<NationalPupilDatabaseLearner> learners = NationalPupilDatabaseLearnerTestDoubles.FakeMany(domainLearners.Count);
+
+        NationalPupilDatabaseSearchResponse response =
+            NationalPupilDatabaseSearchResponseTestDoubles.Create(
+                learners: new NationalPupilDatabaseLearners(learners),
+                facets: SearchFacetsTestDouble.CreateSingleFacetGroup("Region", "North", 1),
+                status: SearchResponseStatus.Success,
+                totalResults: learners.Count);
+
+        Mock<IMapper<NationalPupilDatabaseLearner, Learner>> learnerMapper = new();
+        learnerMapper
+            .Setup((mapper) => mapper.Map(It.Is<NationalPupilDatabaseLearner>(t => t == learners[0])))
+            .Returns(domainLearners[0]);
+        learnerMapper
+            .Setup((mapper) => mapper.Map(It.Is<NationalPupilDatabaseLearner>(t => t == learners[1])))
+            .Returns(domainLearners[1]);
+
+        LearnerNumberSearchViewModel model = new()
+        {
+            PageSize = 1
+        };
+
+
+        NationalPupilDatabaseLearnerNumericSearchMappingContextToViewModelMapper mapper = new(learnerMapper.Object);
+
+        // act
+        LearnerNumberSearchViewModel result = mapper.Map(
+            NationalPupilDatabaseLearnerNumericSearchMappingContext.Create(
+                model, response));
+
+        // assert
+        Assert.Equivalent(domainLearners, result.Learners);
+        Assert.Equal(2, result.Total);
+
+        learnerMapper.Verify(t => t.Map(It.Is<NationalPupilDatabaseLearner>(req => ReferenceEquals(req, learners[0]))), Times.Once());
+        learnerMapper.Verify(t => t.Map(It.Is<NationalPupilDatabaseLearner>(req => ReferenceEquals(req, learners[1]))), Times.Once());
+    }
+}
