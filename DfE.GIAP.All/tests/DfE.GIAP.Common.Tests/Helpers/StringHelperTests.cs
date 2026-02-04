01c65d90af9c85ef097a920eb6c1b974696b9754
aahmed-dfe <Aasim.AHMED@EDUCATION.GOV.UK>
2026-02-04 20:35:39 +0000

    remove unused helpers

--- DfE.GIAP.All/tests/DfE.GIAP.Common.Tests/Helpers/StringHelperTests.cs
+++ DfE.GIAP.All/tests/DfE.GIAP.Common.Tests/Helpers/StringHelperTests.cs
@@ -5,66 +5,38 @@ using System;
 using System.ComponentModel;
 using Xunit;
 
-namespace DfE.GIAP.Common.Tests.Helpers
+namespace DfE.GIAP.Common.Tests.Helpers;
+
+public sealed class StringHelperTests
 {
-    public class StringHelperTests
+    [Fact]
+    public void FormatUpns_returns_null_if_empty_string_given()
     {
-        [Fact]
-        public void FormatUpns_returns_null_if_empty_string_given()
-        {
-            Assert.Null("".FormatLearnerNumbers());
-        }
+        Assert.Null("".FormatLearnerNumbers());
+    }
 
-        [Fact]
-        public void FormatUpns_returns_array_if_upns_exist()
-        {
-            var upnString = "123\r\n234\r\n345 \r";
-            var upnArray = upnString.FormatLearnerNumbers();
-            Assert.Equal("123", upnArray[0]);
-            Assert.Equal("234", upnArray[1]);
-            Assert.Equal("345", upnArray[2]);
-        }
+    [Fact]
+    public void FormatUpns_returns_array_if_upns_exist()
+    {
+        var upnString = "123\r\n234\r\n345 \r";
+        var upnArray = upnString.FormatLearnerNumbers();
+        Assert.Equal("123", upnArray[0]);
+        Assert.Equal("234", upnArray[1]);
+        Assert.Equal("345", upnArray[2]);
+    }
 
-        [Theory]
-        [InlineData("filename.txt", "filename.txt")]
-        [InlineData("filename.txt", "long/path/with/filename.txt")]
-        public void GetMetaDataFileName_returns_file_name(string expected, string test)
-        {
-            Assert.Equal(expected, StringHelper.GetMetaDataFileName(test));
-        }
+    [Theory]
+    [InlineData(TestEnum.TestItem, "different description")]
+    [InlineData(TestEnum.ToStringItem, "ToStringItem")]
+    public void StringValueOfEnum_returns_correct_value(Enum test, string expected)
+    {
+        Assert.Equal(expected, StringHelper.StringValueOfEnum(test));
+    }
 
-        [Theory]
-        [InlineData("filename", "filename.txt")]
-        public void GetMetaDataName_returns_download_name(string expected, string test)
-        {
-            Assert.Equal(expected, StringHelper.GetMetaDataName(test));
-        }
-
-        [Fact]
-        public void EliminateSanitizeDefaultText_returns_empty_if_string_only_contains_nbsp()
-        {
-            Assert.Equal(string.Empty, "&nbsp;".EliminateSanitizeDefaultText());
-        }
-        
-        [Fact]
-        public void EliminateSanitizeDefaultText_returns_input_if_string_contains_text()
-        {
-            Assert.Equal("test", "test".EliminateSanitizeDefaultText());
-        }
-
-        [Theory]
-        [InlineData(TestEnum.TestItem, "different description")]
-        [InlineData(TestEnum.ToStringItem, "ToStringItem")]
-        public void StringValueOfEnum_returns_correct_value(Enum test, string expected)
-        {
-            Assert.Equal(expected, StringHelper.StringValueOfEnum(test));
-        }
-
-        private enum TestEnum
-        {
-            [Description("different description")]
-            TestItem,
-            ToStringItem
-        }
+    private enum TestEnum
+    {
+        [Description("different description")]
+        TestItem,
+        ToStringItem
     }
 }
