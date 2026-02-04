6bd03da71c8f9f0db306875af57ce6bf02bf8622
aahmed-dfe <Aasim.AHMED@EDUCATION.GOV.UK>
2026-02-04 21:58:27 +0000

    back provider onto same opts

--- DfE.GIAP.All/src/DfE.GIAP.Web/Features/MyPupils/PupilSelection/GetPupilSelections/GetMyPupilsPupilSelectionProvider.cs
+++ DfE.GIAP.All/src/DfE.GIAP.Web/Features/MyPupils/PupilSelection/GetPupilSelections/GetMyPupilsPupilSelectionProvider.cs
@@ -1,24 +1,30 @@
-﻿using DfE.GIAP.Web.Shared.Session.Abstraction;
+﻿using DfE.GIAP.Web.Features.MyPupils.PupilSelection.Options;
+using DfE.GIAP.Web.Shared.Session.Abstraction;
 using DfE.GIAP.Web.Shared.Session.Abstraction.Query;
+using Microsoft.Extensions.Options;
 
 namespace DfE.GIAP.Web.Features.MyPupils.PupilSelection.GetPupilSelections;
 
 internal sealed class GetMyPupilsPupilSelectionProvider : IGetMyPupilsPupilSelectionProvider
 {
-    private static readonly SessionCacheKey selectionStateCacheKey = new("MyPupilsPupilSelections");
     private readonly ISessionQueryHandler<MyPupilsPupilSelectionState> _selectionStateSessionQueryHandler;
+    private readonly SessionCacheKey _sessionKey;
 
     public GetMyPupilsPupilSelectionProvider(
-        ISessionQueryHandler<MyPupilsPupilSelectionState> selectionStateSessionQueryHandler)
+        ISessionQueryHandler<MyPupilsPupilSelectionState> selectionStateSessionQueryHandler,
+        IOptions<MyPupilSelectionOptions> options)
     {
         ArgumentNullException.ThrowIfNull(selectionStateSessionQueryHandler);
         _selectionStateSessionQueryHandler = selectionStateSessionQueryHandler;
 
+        ArgumentNullException.ThrowIfNull(options);
+        ArgumentNullException.ThrowIfNull(options.Value);
+        _sessionKey = new(options.Value.SelectionsSessionKey);
     }
 
     public MyPupilsPupilSelectionState GetPupilSelections()
     {
-        SessionQueryResponse<MyPupilsPupilSelectionState> selectionStateResponse = _selectionStateSessionQueryHandler.Handle(selectionStateCacheKey);
+        SessionQueryResponse<MyPupilsPupilSelectionState> selectionStateResponse = _selectionStateSessionQueryHandler.Handle(_sessionKey);
 
         MyPupilsPupilSelectionState selectionState =
             selectionStateResponse.HasValue ?
