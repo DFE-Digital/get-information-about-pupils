c1458d9b50e44328deff9c5edcc54d63d67e90c7
aahmed-dfe <Aasim.AHMED@EDUCATION.GOV.UK>
2026-02-05 21:12:39 +0000

    filescoped namespaces, and remove global mypupils unused cosntants

--- DfE.GIAP.All/src/DfE.GIAP.Web/Helpers/SelectionManager/ITextSearchSelectionManager.cs
+++ DfE.GIAP.All/src/DfE.GIAP.Web/Helpers/SelectionManager/ITextSearchSelectionManager.cs
@@ -1,10 +1,9 @@
-﻿namespace DfE.GIAP.Web.Helpers.SelectionManager
-{
-    public interface ITextSearchSelectionManager
-    {
-        public void Add(string selectedLearnerNumber);
-        public void Clear();
+﻿namespace DfE.GIAP.Web.Helpers.SelectionManager;
 
-        public string GetSelectedFromSession();
-    }
+public interface ITextSearchSelectionManager
+{
+    public void Add(string selectedLearnerNumber);
+    public void Clear();
+
+    public string GetSelectedFromSession();
 }
--- DfE.GIAP.All/src/DfE.GIAP.Web/Helpers/SelectionManager/SelectionManager.cs
+++ DfE.GIAP.All/src/DfE.GIAP.Web/Helpers/SelectionManager/SelectionManager.cs
@@ -5,77 +5,76 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace DfE.GIAP.Web.Helpers.SelectionManager
+namespace DfE.GIAP.Web.Helpers.SelectionManager;
+
+/// <summary>
+/// note that because the default for the UPN/PP/ULN pages is for everything to be selected,
+/// to keep the content of session small, this actually tracks the things that are NOT selected..
+/// </summary>
+public class NotSelectedManager : ISelectionManager
 {
-    /// <summary>
-    /// note that because the default for the UPN/PP/ULN pages is for everything to be selected,
-    /// to keep the content of session small, this actually tracks the things that are NOT selected..
-    /// </summary>
-    public class NotSelectedManager : ISelectionManager
+    public const string NotSelectedKey = "notSelected";
+    private readonly HttpContext context;
+
+    public NotSelectedManager(IHttpContextAccessor contextAccessor)
     {
-        public const string NotSelectedKey = "notSelected";
-        private readonly HttpContext context;
-
-        public NotSelectedManager(IHttpContextAccessor contextAccessor)
+        this.context = contextAccessor.HttpContext;
+    }
+  
+    public void AddAll(IEnumerable<string> upns)
+    {
+        // if it exists, remove it.
+        var inSession = GetFromSession();
+        foreach (var upn in upns)
         {
-            this.context = contextAccessor.HttpContext;
-        }
-      
-        public void AddAll(IEnumerable<string> upns)
-        {
-            // if it exists, remove it.
-            var inSession = GetFromSession();
-            foreach (var upn in upns)
+            if (inSession.Contains(upn))
             {
-                if (inSession.Contains(upn))
-                {
-                    inSession.Remove(upn);
-                }
-            }
-            UpdateSession(inSession);
-        }
-
-        public void RemoveAll(IEnumerable<string> upns)
-        {
-            var inSession = GetFromSession();
-            foreach (var upn in upns)
-            {
-                if (!inSession.Contains(upn))
-                {
-                    inSession.Add(upn);
-                }
-            }
-            UpdateSession(inSession);
-        }
-
-        public void Clear()
-        {
-            if (context.Session.Keys.Contains(NotSelectedKey))
-            {
-                context.Session.Remove(NotSelectedKey);
+                inSession.Remove(upn);
             }
         }
+        UpdateSession(inSession);
+    }
 
-        public HashSet<string> GetSelected(string[] available)
+    public void RemoveAll(IEnumerable<string> upns)
+    {
+        var inSession = GetFromSession();
+        foreach (var upn in upns)
         {
-            // compare available and remove everything that exists in session..
-            var inSession = GetFromSession();
-            return available.Except(inSession).ToHashSet<string>();
-        }
-
-        private void UpdateSession(HashSet<string> notSelected)
-        {
-            context.Session.SetString(NotSelectedKey, JsonConvert.SerializeObject(notSelected));
-        }   
-        
-        private HashSet<string> GetFromSession()
-        {
-            if (context.Session.Keys.Contains(NotSelectedKey))
+            if (!inSession.Contains(upn))
             {
-                return JsonConvert.DeserializeObject<HashSet<string>>(context.Session.GetString(NotSelectedKey));
+                inSession.Add(upn);
             }
-
-            return new HashSet<string>();
         }
+        UpdateSession(inSession);
+    }
+
+    public void Clear()
+    {
+        if (context.Session.Keys.Contains(NotSelectedKey))
+        {
+            context.Session.Remove(NotSelectedKey);
+        }
+    }
+
+    public HashSet<string> GetSelected(string[] available)
+    {
+        // compare available and remove everything that exists in session..
+        var inSession = GetFromSession();
+        return available.Except(inSession).ToHashSet<string>();
+    }
+
+    private void UpdateSession(HashSet<string> notSelected)
+    {
+        context.Session.SetString(NotSelectedKey, JsonConvert.SerializeObject(notSelected));
+    }   
+    
+    private HashSet<string> GetFromSession()
+    {
+        if (context.Session.Keys.Contains(NotSelectedKey))
+        {
+            return JsonConvert.DeserializeObject<HashSet<string>>(context.Session.GetString(NotSelectedKey));
+        }
+
+        return new HashSet<string>();
     }
 }
