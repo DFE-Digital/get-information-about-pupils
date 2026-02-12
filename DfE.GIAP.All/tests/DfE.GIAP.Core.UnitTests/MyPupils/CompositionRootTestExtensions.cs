f62994619264eecac7487dca59358b743bd5d197
aahmed-dfe <119260940+aahmed-dfe@users.noreply.github.com>
2026-02-12 10:17:27 +0000

    tests: Remove Search dependnecies in CompositionRootTests for MyPupils and Fake Port (#610)
    * tidy up composition root tests
    
    * lint

---
+++ DfE.GIAP.All/tests/DfE.GIAP.Core.UnitTests/MyPupils/CompositionRootTestExtensions.cs
@@ -0,0 +1,13 @@
+﻿using DfE.GIAP.Core.MyPupils.Application.Ports;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DfE.GIAP.Core.UnitTests.MyPupils;
+internal static class CompositionRootTestExtensions
+{
+    internal static IServiceCollection AddMyPupilsCoreFakeAdaptors(this IServiceCollection services)
+    {
+        services.AddScoped<IQueryMyPupilsPort>(sp => new Mock<IQueryMyPupilsPort>().Object);
+        return services;
+    }
+
+}
