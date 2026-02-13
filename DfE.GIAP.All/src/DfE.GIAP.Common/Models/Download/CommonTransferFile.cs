77cbd773089e67150dba4f8ff6d1b617e25227db
aahmed-dfe <Aasim.AHMED@EDUCATION.GOV.UK>
2026-02-13 12:56:22 +0000

    format common

--- DfE.GIAP.All/src/DfE.GIAP.Common/Models/Download/CommonTransferFile.cs
+++ DfE.GIAP.All/src/DfE.GIAP.Common/Models/Download/CommonTransferFile.cs
@@ -1,16 +1,15 @@
-﻿using Newtonsoft.Json;
-using System.Diagnostics.CodeAnalysis;
+﻿using System.Diagnostics.CodeAnalysis;
+using Newtonsoft.Json;
 
-namespace DfE.GIAP.Domain.Models.Download
+namespace DfE.GIAP.Domain.Models.Download;
+
+[ExcludeFromCodeCoverage]
+public class CommonTransferFile
 {
-    [ExcludeFromCodeCoverage]
-    public class CommonTransferFile
-    {
-        [JsonProperty("UPNs")]
-        public string[] UPNs { get; set; }
-        public string[] SortOrder { get; set; }
-        public bool IsEstablishment { get; set; }
-        public string LocalAuthorityNumber { get; set; }
-        public string EstablishmentNumber { get; set; }
-    }
+    [JsonProperty("UPNs")]
+    public string[] UPNs { get; set; }
+    public string[] SortOrder { get; set; }
+    public bool IsEstablishment { get; set; }
+    public string LocalAuthorityNumber { get; set; }
+    public string EstablishmentNumber { get; set; }
 }
