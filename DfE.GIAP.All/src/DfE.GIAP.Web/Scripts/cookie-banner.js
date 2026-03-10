2eac8894e230973e9e016144e15123c99df09d4b
TonyPalmerDfE <tony.palmer@education.gov.uk>
2026-03-10 09:24:28 +0000

    refactor: remove unused css and partials (#660)

--- DfE.GIAP.All/src/DfE.GIAP.Web/Scripts/cookie-banner.js
+++
@@ -1,6 +0,0 @@
-﻿if ($('#CookieBanner').length > 0) {
-    $("#btn-accept-cookie").click(function () {
-        document.cookie = $(this).attr('data-cookie-string');
-        $('#CookieBanner').hide();
-    });
-}
