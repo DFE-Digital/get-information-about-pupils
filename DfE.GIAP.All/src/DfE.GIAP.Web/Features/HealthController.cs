434a093aa8cf0953603ee94d29d710bdc81e270b
TonyPalmerDfE <tony.palmer@education.gov.uk>
2026-03-23 11:39:12 +0000

    feat: health endpoint (#671)

---
+++ DfE.GIAP.All/src/DfE.GIAP.Web/Features/HealthController.cs
@@ -0,0 +1,12 @@
+﻿using DfE.GIAP.Web.Middleware;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DfE.GIAP.Web.Features;
+
+[AllowAnonymous]
+[AllowWithoutConsent]
+public class HealthController : Controller
+{
+    public IActionResult Index() => Ok("Ok");
+}
